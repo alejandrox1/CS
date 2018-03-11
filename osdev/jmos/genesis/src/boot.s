f7171dcc6d29f4b1bd0884a27704a52f89b0880d
alejandrox1 <alarcj137@gmail.com>
2018-03-11 13:41:20 -0400

    changes:osdev/jmos/genesis/src/boot.s added more comments to multiboot header flags

--- osdev/jmos/genesis/src/boot.s
+++ osdev/jmos/genesis/src/boot.s
@@ -7,9 +7,11 @@
 ;
 MBOOT_PAGE_ALIGN   equ 1<<0       ; Load kernel and modules on a page boundary.
 MBOOT_MEM_INFO     equ 1<<1       ; Provide your kernel with memory info.
-MBOOT_HEADER_MAGIC equ 0x1BADB002 ; Multiboot magic number.
+MBOOT_HEADER_MAGIC equ 0x1BADB002 ; Multiboot magic number. Identifies kernel 
+                                  ; multiboot compatible.
 ; We do not use MBOOT_ABOUT_KLUDGE. This means GRUB does not pass us a symbol 
-; table.
+; table. Because we willbe using elf format, this is not necessary and adding 
+; it would stop GRUB from giving us a symbol table when we boot up.
 MBOOT_HEADER_FLAGS equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
 MBOOT_CHECKSUM     equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
 
@@ -22,7 +24,8 @@ MBOOT_CHECKSUM     equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
 
 mboot:
 	dd MBOOT_HEADER_MAGIC ; GRUB will search for this value on each 4-byte 
-	                      ; boundary in the kernel file.
+	                      ; boundary in the kernel file. Identifies kernel as 
+                          ; multiboot compatible
 	dd MBOOT_HEADER_FLAGS ; How GRUB should load file/settings.
 	dd MBOOT_CHECKSUM     ; Ensure the above values are correct.
 
