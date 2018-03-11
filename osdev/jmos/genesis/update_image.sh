8a59232b1d689744b510ab18a425d8def1e523d4
alejandrox1 <alarcj137@gmail.com>
2018-03-11 14:55:51 -0400

    changes:osdev/jmos/genesis/update_image.sh update script to update floppy img and clean after itself

--- osdev/jmos/genesis/update_image.sh
+++ osdev/jmos/genesis/update_image.sh
@@ -11,4 +11,7 @@ sudo losetup /dev/loop0 floppy.img
 sudo mount /dev/loop0 mnt/
 sudo cp src/kernel /mnt/kernel
 sudo umount /dev/loop0
-sudo losetup -d /dev/loop0 
+sudo losetup -d /dev/loop0
+
+rm -r mnt
+make -C src/ clean
