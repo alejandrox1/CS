31cc0505cefeb0c1623ade213fd06ce4ec86509f
alejandrox1 <alarcj137@gmail.com>
2018-03-12 14:57:49 -0400

    changes:osdev/jmos/screen/src/common.h fixed types refs in common.c

--- osdev/jmos/screen/src/common.c
+++ osdev/jmos/screen/src/common.c
@@ -8,7 +8,7 @@
 /* 
  * outb writes a byte out to the specified port.
  */
-void outb(u16int port, u8int value)
+void outb(u16int_t port, u8int_t value)
 {
 	asm volatile("outb %1, %0" : : "dN" (port), "a" (value));
 }
@@ -16,9 +16,9 @@ void outb(u16int port, u8int value)
 /*
  * inb reads a byte from the specified port.
  */
-u8int inb(u16int port)
+u8int inb(u16int_t port)
 {
-	u8int ret;
+	u8int_t ret;
 	asm volatile("inb %1, %0" : "=a" (ret), : "dN" (port));
 	return ret;
 }
@@ -26,9 +26,9 @@ u8int inb(u16int port)
 /*
  * inw reads a word (16 bits) from the specified port.
  */
-u16int inw(u16int port)
+u16int inw(u16int_t port)
 {
-	u16int ret;
+	u16int_t ret;
 	asm volatile("inw %1, %0" : "=a" (ret) : "dN" (port));
 	return ret;
 }
