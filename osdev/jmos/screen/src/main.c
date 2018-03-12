8c87b47815efee9fb4c767b3a1190c6b086dc23c
alejandrox1 <alarcj137@gmail.com>
2018-03-12 17:03:35 -0400

    changes:osdev/jmos/screen/src/main.c kernel entrypoint will now salute us

--- osdev/jmos/screen/src/main.c
+++ osdev/jmos/screen/src/main.c
@@ -4,9 +4,14 @@
  * Define the kernel entrypoint and calls initialization routines.
  * From James Molloy kernel dev tutorial.
  */
+#include "monitor.h"
+
 
 int main(struct multiboot *mboot_ptr)
 {
+	monitor_clear();
+	monitor_write("Hello, world!");
+
 	// Alll initialization calls go in here.
 	return 0xDEADBABA;
 }
