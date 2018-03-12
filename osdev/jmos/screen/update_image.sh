506860170311fc56cc82ffb62818efd5e5558303
alejandrox1 <alarcj137@gmail.com>
2018-03-12 12:29:46 -0400

    changes:osdev/jmos/screen added code base for screen driver implementation

---
+++ osdev/jmos/screen/update_image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+make -C src/ 
+
+if [ -f src/kernel ]; then
+	mkdir -p mnt/ && cp src/kernel mnt/kernel; 
+fi
+
+ 
+sudo losetup /dev/loop0 floppy.img
+sudo mount /dev/loop0 mnt/
+sudo cp src/kernel /mnt/kernel
+sudo umount /dev/loop0
+sudo losetup -d /dev/loop0
+
+rm -r mnt
+make -C src/ clean
