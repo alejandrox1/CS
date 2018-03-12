16e2e7a7c78ddfec55d3bac43228c9748a1c3613
alejandrox1 <alarcj137@gmail.com>
2018-03-12 18:35:18 -0400

    changes:sdev/jmos/genesis/update_image.sh fixed bug - cping kernel to wrong dest

--- osdev/jmos/genesis/update_image.sh
+++ osdev/jmos/genesis/update_image.sh
@@ -11,7 +11,7 @@ fi
  
 sudo losetup /dev/loop0 floppy.img
 sudo mount /dev/loop0 mnt/
-sudo cp src/kernel /mnt/kernel
+sudo cp src/kernel mnt/kernel
 sudo umount /dev/loop0
 sudo losetup -d /dev/loop0
 
