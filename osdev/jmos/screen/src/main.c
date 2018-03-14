5f9c9a6b49bdde048be36ee05bfaa30086da426c
alejandrox1 <alarcj137@gmail.com>
2018-03-13 20:30:52 -0400

    changes:osdev/jmos/screen/main.c added more examples to test all write funcs

--- osdev/jmos/screen/src/main.c
+++ osdev/jmos/screen/src/main.c
@@ -11,7 +11,28 @@ int main()//struct multiboot *mboot_ptr)
 {
 	monitor_clear();
 	monitor_write("Hello, world!");
+	monitor_write("\nGoing to try and print a couple numbers:\n");
+	
+	monitor_write("\tprint 1 : ");
+	monitor_write_dec(1);
+	monitor_write("\tprint 10 : ");                                              
+	monitor_write_dec(10);
+	monitor_write("\tprint 110 : ");                                              
+	monitor_write_dec(110);
+
+	monitor_write("\n");
+
+	monitor_write("\tprint 0x0 : ");                                              
+	monitor_write_hex(0x0); 
+	monitor_write("\tprint 0x5 : ");                                            
+	monitor_write_hex(0x5);
+	monitor_write("\tprint 0xA : ");                                             
+	monitor_write_hex(0xA);                                                     
+	monitor_write("\tprint 0xFF : ");                                            
+	monitor_write_hex(0xFF);
+
+	monitor_write("\n");
 
 	// All initialization calls go in here.
-	return 0x12345678;
+	return 0x12345677;
 }
