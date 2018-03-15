18ea7cb90802bd9f9af8fbc5befde9836c5f6ad1
alejandrox1 <alarcj137@gmail.com>
2018-03-15 14:12:09 -0400

    changes:osdev/jmos/gdt-ivt/src/main.c small gdt/idt tests

--- osdev/jmos/gdt-ivt/src/main.c
+++ osdev/jmos/gdt-ivt/src/main.c
@@ -4,11 +4,22 @@
  * Define the kernel entrypoint and calls initialization routines.
  * From James Molloy kernel dev tutorial.
  */
+#include "gdt.h"
+#include "idt.h"
 #include "monitor.h"
+
+static void int_3_handler(regsiters_t *regs)
+{
+	monitor_write("Interrupt 3 received!\n");
+}
 
 
 int main()//struct multiboot *mboot_ptr)
 {
+
+	/*
+	 * Test monitor.h
+	 */
 	monitor_clear();
 	monitor_write("Hello, world!");
 	monitor_write("\nGoing to try and print a couple numbers:\n");
@@ -32,6 +43,18 @@ int main()//struct multiboot *mboot_ptr)
 	monitor_write_hex(0xFF);
 
 	monitor_write("\n");
+
+	/*
+	 * Test IDT
+	 */
+	init_gdt();
+	init_idt();
+
+	register_interrupt_handler(3, &int_3_handler);
+
+	asm volatile("int $0x3");
+	asm volatile("int $0x4");
+
 
 	// All initialization calls go in here.
 	return 0x12345677;
