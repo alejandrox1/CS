ad37287f07d80f72ea6d12d45450965c34ff224d
alejandrox1 <alarcj137@gmail.com>
2018-03-16 11:47:27 -0400

    changes:osdev/jmos/04/src/idt.[h|c|s] added IRQs

--- osdev/jmos/04-irqs_pit/src/idt_s.s
+++ osdev/jmos/04-irqs_pit/src/idt_s.s
@@ -19,7 +19,7 @@ idt_flush:
 %macro ISR_NOERRCODE 1
     global isr%1
 	isr%1:
-	    cli
+        cli
 		push 0     ; Push dummy error code.
 		push %1    ; Push the interrupt number.
 		jmp isr_common_stub 
@@ -31,10 +31,25 @@ idt_flush:
 %macro ISR_ERRCODE 1
     global isr%1
 	isr%1:
-	    cli
+        cli
 		push %1    ; Push the interrupt number.
 		jmp isr_common_stub
 %endmacro
+
+;
+; Macro creates a stub for an IRQ.
+; The first parameter is the IRQ number, the second is the ISR number it is 
+; remapped to.
+'
+%macro IRQ 2
+    global irq%1
+    irq%1:
+        cli
+        push byte 0
+        push byte %2
+        jmp irq_common_stub
+%endmacro
+    
 
 ISR_NOERRCODE 0                                                                 
 ISR_NOERRCODE 1                                                                 
@@ -69,12 +84,29 @@ ISR_NOERRCODE 29
 ISR_NOERRCODE 30                                                                
 ISR_NOERRCODE 31                                                                
 ISR_NOERRCODE 255
+IRQ   0,    32                                                                  
+IRQ   1,    33                                                                  
+IRQ   2,    34                                                                  
+IRQ   3,    35                                                                  
+IRQ   4,    36                                                                  
+IRQ   5,    37                                                                  
+IRQ   6,    38                                                                  
+IRQ   7,    39                                                                  
+IRQ   8,    40                                                                  
+IRQ   9,    41                                                                  
+IRQ  10,    42                                                                  
+IRQ  11,    43                                                                  
+IRQ  12,    44                                                                  
+IRQ  13,    45                                                                  
+IRQ  14,    46                                                                  
+IRQ  15,    47 
 
 
 ;
 ; extern functions
 ;
 extern idt_handler
+extern irq_handler
 
 ;
 ; isr_common_stub saves the processor's state, sets up the kernel mode 
@@ -92,7 +124,7 @@ isr_common_stub:
 	mov fs, ax
 	mov gs, ax
 
-	push esp,        ; Push a pointer to the current top of the stack. This 
+	push esp         ; Push a pointer to the current top of the stack. This 
 	                 ; will become the register_t* parameter.
 	call idt_handler ; C routine (idt.c).
 	add esp, 4       ; Remove the pointer to the stack from the stack (ha!).
@@ -104,5 +136,35 @@ isr_common_stub:
 	mov gs, bx
 
 	popa
-	add esp, 8       ; Clean up the pushed error code and ISRnumber.
+	add esp, 8       ; Clean up the pushed error code and ISR number.
 	iret             ; pop CS, SS, ESP, EIP, and EFLAGS.
+
+
+;
+; irq_common_stub saves the processor state, sets up the kernel mode segments,
+; calls the C-level fault handler, and finally restores the stack frame.
+;
+irq_common_stub:
+    pusha
+    mov ax, ds
+    push eax
+
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    push esp
+    call irq_handler
+    add esp, 4
+
+    pop ebx
+    mov ds, bx
+    mov es, bx
+    mov fs, bx
+    mov gs, bx
+
+    popa
+    add esp, 8 
+    iret
