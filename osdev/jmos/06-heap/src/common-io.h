db8979f889dcc86cfb6528aeaefa49924b6de3c3
alejandrox1 <alarcj137@gmail.com>
2018-03-21 20:09:04 -0400

    changes:osdev/jmos/06/src/common-io.h added assert macro

--- osdev/jmos/06-heap/src/common-io.h
+++ osdev/jmos/06-heap/src/common-io.h
@@ -16,4 +16,7 @@ void _monitor_trace(const char *file,  int32_t line, const char *msg)
 
 #define monitor_trace(msg) _monitor_trace(__FILE__, __LINE__, (msg))
 
+#define ERRMSG "Error occurred -"                                               
+#define ASSERT(expr) if (!(expr)) monitor_trace(ERRMSG)
+
 #endif
