ec2448fac4f65b977fc972c7394b8c5a6c9ac6bb
alejandrox1 <alarcj137@gmail.com>
2018-03-26 20:23:29 -0400

    changes:osdev/jmos/06-heap/src/kheap.[hc] added kernel heap impl

--- osdev/jmos/06-heap/src/kheap.h
+++ osdev/jmos/06-heap/src/kheap.h
@@ -9,6 +9,8 @@
 #define KHEAP_H
 
 #include "common.h"
+#include "ord_array.h"
+
 
 #define KHEAP_START         0xC0000000 // 3GB == 12* 16**7 / (1024**3)
 #define KHEAP_INITIAL_SIZE  0x100000   // 1MB
@@ -55,5 +57,30 @@ void *alloc(uint32_t size, uint8_t page_align, heap_t *heap);
 
 // free releases a block allocated with alloc.
 void free(void *p, heap_t *heap);
+
+
+/*
+ * Memory Allocation Functions.
+ */
+// kmalloc_int allocates a chunk of memory of size sz. If align == 1, the chunk
+// will be page aligned. If phys != 0, the physical location of the allocated
+// chunk will be stored in phys.
+uint32_t kmalloc_int(uint32_t sz, int8_t align, uint32_t *phys);
+
+// kmalloc is a general purpose memory allocation function.
+uint32_t kmalloc(uint32_t sz);
+
+// kmalloc_a will page aligned the allocated memory.
+uint32_t kmalloc_a(uint32_t sz);
+
+// kmalloc_p saves the physical address.
+uint32_t kmalloc_p(uint32_t sz, uint32_t *phys);
+
+// kmalloc_ap page aligns the allocated memory and saves physical address.
+uint32_t kmalloc_ap(uint32_t sz, uint32_t *phys);
+
+// kfree is a general purpose memory deallocation function.
+void kfree(void *p);
+
 
 #endif // KHEAP_H
