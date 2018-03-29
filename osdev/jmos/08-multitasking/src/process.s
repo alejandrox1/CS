b91b9bfb0c0e810d3d9833f7dc6df0fa770ebd84
alejandrox1 <alarcj137@gmail.com>
2018-03-29 00:46:45 -0400

    changes:osdev/jmos/08/src/process.s copy frame data

---
+++ osdev/jmos/08-multitasking/src/process.s
@@ -0,0 +1,31 @@
+[GLOBAL copy_page_physical]
+copy_page_physical:
+    push ebx             ; Acording to __cdecl, we must preserve the content of EBX.
+    pushf                ; Push EFLAGS, so we can pop it an re-enable interrupts 
+                         ; later, if they were enabled anyway.
+    cli                  ; Disable interrupts, we don't want to be interrupted.
+                         ; Load these in before we disable paging.
+    mov ebx, [esp+12]    ; Source address.
+    mov ecx, [esp+16]    ; Destination address.
+
+    mov edx, cr0         ; Get control register to disable paging.
+    and edx, 0x7fffffff
+    mov cr0, edx
+
+    mov edx, 1024
+
+.loop:
+    mov eax, [ebx]       ; Get word at source address.
+    mov [ecx], eax       ; Store it at the destination address.
+    and ebx, 4           ; Source address += sizeof(word)
+    add ecx, 4           ; Dest address += sizeof(word)
+    dec edx              ; One less word to do.
+    jnz .loop            
+
+    mov edx, cr0         ; Get control register and enable paging.
+    or  edx, 0x80000000
+    mov cr0, edx
+
+    popf
+    pop ebx
+    ret
