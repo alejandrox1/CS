dd5909093e25df32f24e95f50c1c254dba85eb2d
alejandrox1 <alarcj137@gmail.com>
2018-03-29 15:27:26 -0400

    changes:osdev/jmos/08/src/task.c implemented forking

--- osdev/jmos/08-multitasking/src/task.c
+++ osdev/jmos/08-multitasking/src/task.c
@@ -11,10 +11,93 @@
 extern page_directory_t *kernel_directory;
 extern page_directory_t *current_directory;
 extern uint32_t          initial_esp;
+extern uint32_t          read_eip();
+
+volatile task_t *current_task;
+volatile task_t *ready_queue;   // Start or task linked list.
+uint32_t         next_pid = 1;
+
 
 /******************************************************************************
  *                               Public API                                   *
  *****************************************************************************/
+/*
+ * initialise_tasking
+ */
+void initialise_tasking()
+{
+    // Rather important stuff happening, no interrupts please!
+    asm volatile("cli");
+
+    // Rellocate the stack just to now where it is.
+    move_stack((void *)0xE0000000, 0x2000);
+
+    // Initialise the first stack (kernel stack).
+    current_task = ready_queue = (task_t *)kmalloc(sizeof(task_t));
+    current_task->id             = next_pid++;
+    current_task->esp            = 0;
+    current-Task->ebp            = 0;
+    current_task->eip            = 0;
+    current_task->page_directory = current_directory;
+    current_task->next           = 0;
+
+    // Get them interrupts back.
+    asm volatile("sti");
+}
+
+/*
+ * fork
+ */
+int fork()
+{
+    // We are going to modify some kernel structures...
+    asm volatile("cli");
+
+    // Take a pointer to this process' task struct for later reference.
+    task_t *parent_task = (task_t *)current_task;
+
+    // Clone address space.
+    page_directory_t *directory = clone_directory(current_directory);
+
+    // Create a new process.
+    task_t new_task = (task_t *)kmalloc(sizeof(task_t));
+    new_task->id             = next_pid++;
+    new_task->esp            = 0;
+    new_task->ebp            = 0;
+    new_task->eip            = 0;
+    new_task->page_directory = directory;
+    new_task->next           = 0;
+
+    // Add it to the end of the ready queue.
+    task_t *tmp_task = (task_t *)ready_queue;
+    while (tmp_task->next)
+        tmp_task = tmp_task->next;
+    tmp_task->next = new_task;
+
+    // This will be the entrypoint for the new process.
+    uint32_t eip = read_eip();
+
+    // Check whether we are the parent or the child.
+    if (current_task == parent_task)
+    {
+        // We are the parent, so set up ESP, EBP, and EIP for child.
+        uint32_t esp, ebp;
+        asm volatile("mov %%esp, %0" : "=r" (esp));
+        asm volatile("mov %%ebp, %0" : "=r" (ebp));
+        new_task->esp = esp;
+        new_task->ebp = ebp;
+        new_task->eip = eip;
+        asm volatile("sti");
+
+        return new_task->id;
+    }
+    else
+    {
+        // We are the child.
+        return 0;
+    }
+}
+
 /*
  * move_stack
  */
