0bfe53e4ec78e84521eb5820f3653cc810b93bd8
alejandrox1 <alarcj137@gmail.com>
2018-03-29 22:24:32 -0400

    changes:osdev/jmos/08/src/main.c updated main

--- osdev/jmos/08-multitasking/src/main.c
+++ osdev/jmos/08-multitasking/src/main.c
@@ -1,81 +1,88 @@
-/* 
- * main.c
- *
- * Define the kernel entrypoint and calls initialization routines.
- * From James Molloy kernel dev tutorial.
- */
-#include "types.h"
-#include "multiboot.h"
+// main.c -- Defines the C-code kernel entry point, calls initialisation routines.
+//           Made for JamesM's tutorials <www.jamesmolloy.co.uk>
+
+#include "monitor.h"
 #include "gdt.h"
 #include "idt.h"
 #include "timer.h"
 #include "paging.h"
-#include "kheap.h"
+#include "multiboot.h"
+#include "fs.h"
 #include "initrd.h"
-#include "monitor.h"
+#include "task.h"
 #include "kassert.h"
 
 extern uint32_t placement_address;
-
 uint32_t initial_esp;
-
-
 
 int kmain(struct multiboot *mboot_ptr, uint32_t initial_stack)
 {
     initial_esp = initial_stack;
+    // Initialise all the ISRs and segmentation
+    init_gdt();
+    init_idt();
+    // Initialise the screen (by clearing it)
+    monitor_clear();
+    // Initialise the PIT to 100Hz
+    asm volatile("sti");
+    init_timer(50);
 
-	/*
-	 * Test IDT
-	 */
-	init_gdt();
-	init_idt();
-
-
-    /*
-     * initrd
-     */
-    // Find the location of our initrd.
+    // Find the location of our initial ramdisk.
     KASSERT(mboot_ptr->mods_count > 0);
-    uint32_t initrd_location = *((uint32_t *)mboot_ptr->mods_addr);
-    uint32_t initrd_end      = *(uint32_t *)(mboot_ptr->mods_addr+4);
-    
-    // Do not trample our module with placement address.
+    uint32_t initrd_location = *((uint32_t*)mboot_ptr->mods_addr);
+    uint32_t initrd_end = *(uint32_t*)(mboot_ptr->mods_addr+4);
+    // Don't trample our module with placement accesses, please!
     placement_address = initrd_end;
 
-    //uint32_t a = kmalloc(8);     
-    initialise_paging(); 
+    // Start paging.
+    initialise_paging();
 
-    monitor_write("\n\n");
+    // Start multitasking.
+    initialise_tasking();
+
     // Initialise the initial ramdisk, and set it as the filesystem root.
     fs_root = initialize_initrd(initrd_location);
 
-    // list the contents of /                                                   
-    int i = 0;     
-    struct dirent *node = 0;    
-    while ((node = readdir_fs(fs_root, i)) != 0)  
-    {     
-        monitor_write("Found file ");           
-        monitor_write(node->name);      
-        fs_node_t *fsnode = finddir_fs(fs_root, node->name);        
-       
-        if ((fsnode->flags&0x7) == FS_DIRECTORY)     
-        {     
-            monitor_write("\n\t(directory)\n");   
-        }    
-        else 
-        {     
-            monitor_write("\n\t contents: \"");   
-            char buf[256];    
-            uint32_t sz = read_fs(fsnode, 0, 256, (uint8_t *)buf);     
-            int j;    
-            for (j = 0; j < sz; j++)       
-                monitor_put(buf[j]);   
-            monitor_write("\"\n");    
-        }     
-        i++;                                                                    
+    // Create a new process in a new address space which is a clone of this.
+    int ret = fork();
+
+    monitor_write("fork() returned ");
+    monitor_write_hex(ret);
+    monitor_write(", and getpid() returned ");
+    monitor_write_hex(getpid());
+    monitor_write("\n============================================================================\n");
+
+    // The next section of code is not reentrant so make sure we aren't interrupted during.
+    asm volatile("cli");
+    // list the contents of /
+    int i = 0;
+    struct dirent *node = 0;
+    while ( (node = readdir_fs(fs_root, i)) != 0)
+    {
+        monitor_write("Found file ");
+        monitor_write(node->name);
+        fs_node_t *fsnode = finddir_fs(fs_root, node->name);
+
+        if ((fsnode->flags&0x7) == FS_DIRECTORY)
+        {
+            monitor_write("\n\t(directory)\n");
+        }
+        else
+        {
+            monitor_write("\n\t contents: \"");
+            char buf[256];
+            uint32_t sz = read_fs(fsnode, 0, 256, (uint8_t *)buf);
+            int j;
+            for (j = 0; j < sz; j++)
+                monitor_put(buf[j]);
+            
+            monitor_write("\"\n");
+        }
+        i++;
     }
-    
-    // All initialization calls go in here.
-	return 0x12345677;
+    monitor_write("\n");
+
+    asm volatile("sti");
+
+    return 0;
 }
