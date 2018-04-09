5737b43448c57c485f085377299d530ea9f606e9
alejandrox1 <alarcj137@gmail.com>
2018-04-09 15:51:55 -0400

    osdev/jmos/09-usermode/src/gdt.c implemented write_tss to initialise a TSS segment

--- osdev/jmos/09-usermode/src/gdt.c
+++ osdev/jmos/09-usermode/src/gdt.c
@@ -15,13 +15,15 @@ gdt_entry_t gdt_entries[ENTRIES];
 gdt_ptr_t gdt_ptr;
 
 
-// gdt_flush sets up the GDT. 
-// Call to load GDT must be done in assembly.
+// gdt_flush sets up the GDT. Call to load GDT must be done in assembly.
 extern void gdt_flush(uint32_t);
+// tss_flush
+extern void tss_flush();
 
 // gdt_set_gate
 static void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran);
-
+// write_tss 
+static void wite_tss(int32_t num, uint16_t ss0, uint32_t esp0);
 
 /******************************************************************************
  *                              Public API                                    *
@@ -75,4 +77,34 @@ static void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t acc
 
 	gdt_entries[num].granularity |= gran & 0xF0;
 	gdt_entries[num].access      =  access;
+}
+
+/* 
+ * write_tss initialises our tast state segment struct.
+ */
+static void write_tss(int32_t num, uint16_t ss0, uint32_t esp0)
+{
+    // Compute the base and limit of our entry into the GDT.
+    uint32_t base  = (uint32_t)&tss_entry;
+    uint32_t limit = base + sizeof(tss_entry);
+
+    // Add TSS descriptor's address into the GDT.
+    get_set_gate(num, base, limit, 0xE9, 0x00);
+
+    // Ensure the descriptor is initially zero
+    memset((uint8_t *)&tss_entry, 0, sizeof(tss_entry));
+
+    tss_entry.ss0  = ss0;  // Set the kernel stack segment.
+    tss_entry.esp0 = esp0; // Set the kernel stack pointer.
+
+    /*
+     * Set cs, ds, ss, es, fs, and gs entries in the TSS. These specify what
+     * segments should be loaded when the processor switches to kernel mode.
+     * They are our normal kernel code/data segments, 0x08 and 0x10
+     * respectively, with the last 2 bits set, resulting in 0x0b and 0x13.
+     * Setting these bits sets the RPL (request privilege level) to 3, meaning
+     * this TSS can be used to switch to kernel mode from ring 3.
+     */
+    tss_entry.cs = 0x0b;
+    tss_entry.ss = tss_entry.ds = tss_entry.es = tss_entry.fs = tss_entry.gs = 0x13;
 }
