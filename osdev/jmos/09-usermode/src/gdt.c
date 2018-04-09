4fe32a8d4e0dd7c099f7ead00d4a27caf3aad677
alejandrox1 <alarcj137@gmail.com>
2018-04-09 16:47:13 -0400

    osdev/jmos/09-usermode/src/gdt.c updated init_gdt() method to use TSS

--- osdev/jmos/09-usermode/src/gdt.c
+++ osdev/jmos/09-usermode/src/gdt.c
@@ -38,21 +38,24 @@ void init_gdt()
 	gdt_ptr.limit = sizeof(gdt_entry_t) * ENTRIES - 1;
 	gdt_ptr.base = (uint32_t)&gdt_entries;
 	
-	gdt_set_gate(0, 0, 0, 0, 0);             // Null segment.                    
-	gdt_set_gate(1, 0, 0xFFFFF, 0x9A, 0xCF); // Code segment.                    
-	gdt_set_gate(2, 0, 0xFFFFF, 0x92, 0xCF); // Data segment.                    
-	gdt_set_gate(3, 0, 0xFFFFF, 0xFA, 0xCF); // User mode code segment.
-	//gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment
+	gdt_set_gate(0, 0, 0, 0, 0);                // Null segment.
+	gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment.
+	gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment.
+	gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User mode code segment.
+	gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment.
+    write_tss(5, 0x10, 0x0);
 
 	gdt_flush((uint32_t)&gdt_ptr);
+    tss_flush();
 }
 
 /*
- * set_kernel_stack
+ * set_kernel_stack updates the TSS entry when we change tasks, it holds the
+ * correct address for the kernel stack.
  */
 void set_kernel_stack(uint32_t stack)
 {
-    return;
+    tss_entry.esp0 = stack;
 }
 
 /******************************************************************************
