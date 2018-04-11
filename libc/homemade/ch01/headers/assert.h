f1cc6d73ff073151ee0883cc800ad14f5bb0a177
alejandrox1 <alarcj137@gmail.com>
2018-04-10 23:47:28 -0400

    libc/homemade/ch01/assert/xassert.c implementation of _Assert

---
+++ libc/homemade/ch01/headers/assert.h
@@ -0,0 +1,19 @@
+#ifndef _ASSERT_H
+#define _ASSERT_H
+
+#undef assert    /* Remove existing definition. */
+
+#ifdef NDEBUG
+    /* Passive form. */
+    #define assert(test) ((void)0)             
+#else
+    void _Assert(char *);
+    /* Macros. */
+    #define _STR(X) _VAL(X)
+    #define _VAL(X) #X
+    /* Active form. */
+    #define assert(test) ((test) ? (void)0  \
+            : _Assert(__FILE__ ":" _STR(__LINE__) " " #test))
+#endif
+
+#endif
