2eda4147de4ec1934ce5c10c840da8d863cf7ca3
alejandrox1 <alarcj137@gmail.com>
2018-04-12 22:48:50 -0400

    libc/ch02-ctype/local/ctype/test_ctype.c added tests for ctype

---
+++ libc/ch02-ctype/local/ctype/test_ctype.c
@@ -0,0 +1,117 @@
+/* test_ctype.c */
+#include <ctype.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+
+/*
+ * prclass displays a printable character class.
+ */
+static void prclass(const char *name, int (*fn)(int))
+{
+    int c;
+
+    fputs(name, stdout);
+    fputs(": ", stdout);
+    for (c = EOF; c <= UCHAR_MAX; ++c)
+        if ((*fn)(c))
+            fput(c, stdout);
+    fputs("\n", stdout);
+}
+
+
+int main()
+{
+    char *s;
+    int c;
+
+    /* Display printable classes. */
+    prclass("ispunct",  &ispunct);
+    prclass("isdigit",  &isdigit);
+    prclass("isxdigit", &isxdigit);
+    prclass("islower",  &islower);
+    prclass("isupper",  &isupper);
+    prclass("isalpha",  &isalpha);
+    prclass("isalnum",  &isalnum);
+
+    /* Test macros for required characters. */
+    for (s = "!\"#%&'();<=>?[\\]*+,-./:^_{|}~"; *s; ++s)
+        assert(ispunct(*s));
+    for (s = "0123456789"; *s; ++s)
+        assert(isdigit(*s) && isxdigit(*s));
+    for (s = "abcdefABCDEF"; *s; ++s)
+        assert(isxdigit(*s));
+    for (s = "abcdefghijklmnopqrstuvwxyz"; *s; ++s)
+        assert(islower(*s));
+    for (s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; *s; ++s)
+        assert(isupper(*s));
+    for (s = "\f\n\r\t\v"; *s; ++s)
+        assert(isspace(*s) && iscntrl(*s));
+    assert(isspace(' ')  && isprint(' '));
+    assert(iscntrl('\a') && iscntrl('\b'));
+
+    /* Test macros for all valid codes. */
+    for (c = EOF; c <= UCHAR_MAX; ++c)
+    {   /* test for proper class membership */
+        if (isdigit(c))
+            assert(isalnum(c));
+        if (isupper(c))
+            assert(isalpha(c));
+        if (islower(c))
+            assert(isalpha(c));
+        if (isalpha(c))
+            assert(isalnum(c) && !isdigit(c));
+        if (isalnum(c))
+            assert(isgraph(c) && !ispunct(c));
+        if (ispunct(c))
+            assert(isgraph(c));
+        if (isgraph(c))
+            assert(isprint(c));
+        if (isspace(c))
+            assert(c == ' ' || !isprint(c));
+        if (iscntrl(c))
+            assert(!isalnum(c));
+    }
+
+    /* Test functions for required characters. */
+    for (s = "0123456789"; *s; ++s)
+        assert((isdigit)(*s) && (isxdigit)(*s));
+    for (s = "abcdefABCDEF"; *s; ++s)
+        assert((isxdigit)(*s));
+    for (s = "abcdefghijklmnopqrstuvwxyz"; *s; ++s)
+        assert((islower)(*s));
+    for (s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; *s; ++s)
+        assert((isupper)(*s));
+    for (s = "!\"#%&'();<=>?[\\]*+,-./:^_{|}~"; *s; ++s)
+        assert((ispunct)(*s));
+    for (s = "\f\n\r\t\v"; *s; ++s)
+        assert((isspace)(*s) && (iscntrl)(*s));
+    assert((isspace)(' ') && (isprint)(' '));
+    assert((iscntrl)('\a') && (iscntrl)('\b'));
+    
+    /* test functions for all valid codes */
+    for (c = EOF; c <= UCHAR_MAX; ++c)
+    {   /* test for proper class membership */
+        if ((isdigit)(c))
+            assert((isalnum)(c));
+        if ((isupper)(c))
+            assert((isalpha)(c));
+        if ((islower)(c))
+            assert((isalpha)(c));
+        if ((isalpha)(c))
+            assert((isalnum)(c) && !(isdigit)(c));
+        if (isalnum(c))
+            assert((isgraph)(c) && !(ispunct)(c));
+        if ((ispunct)(c))
+            assert((isgraph)(c));
+        if ((isgraph)(c))
+            assert((isprint)(c));
+        if ((isspace)(c))
+            assert(c == ' ' || !(isprint)(c));
+        if ((iscntrl)(c))
+            assert(!(isalnum)(c));
+    }
+    
+    puts("SUCCESS testing <ctype.h>");
+    return (0);
+}
