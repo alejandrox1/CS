756f052de6762cf5232042ab08a8f5565d076e3d
alejandrox1 <alarcj137@gmail.com>
2018-04-17 19:48:02 -0400

    llvm/tutorials/01-paser_ast/main.cpp complete listing

--- llvm/tutorials/01-paser_ast/main.cpp
+++ llvm/tutorials/01-paser_ast/main.cpp
@@ -1,7 +1,9 @@
 #include "llvm/ADT/STLExras.h"
+#include <algorithm>
 #include <cctype>
 #include <cstdio>
 #include <cstdlib>
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
@@ -210,6 +212,12 @@ static int getNextToken()
 }
 
 /*
+ * BinopPrecedence - This holds the precedence for each binary operator that is
+ * defined.
+ */
+static std::map<char, int> BinoPrecedence;
+
+/*
  * LogError helper function for error handling. Returns a null unique_ptr of
  * ExprASTs.
  */
@@ -227,6 +235,22 @@ std::unique_ptr<PrototypeAST> LogErrorP(const char *Str)
 {
     LogError(Str);
     return nullptr;
+}
+
+/*
+ * GetTokPrecedence - Get the precedence of the pending binary token.
+ */
+static int GetTokPrecendece()
+{
+    if (!isascii(CurTok))
+        return -1;
+
+    // Make sure it's a declared binop.
+    int TocPrec = BinoPrecedence[CurTok];
+    if (TocPrec <= 0) 
+        return -1;
+
+    return TocPrek;
 }
 
 /*
@@ -315,4 +339,198 @@ static std::unique_ptr<ExprAST> ParsePrimary()
     }
 }
 
+/*
+ * ParseExpression 
+ */
+static std::unique_ptr<ExprAST> ParseExpression()
+{
+    auto LHS = ParsePrimary();
+    if (!LHS)
+        return nullptr;
 
+    return ParseBinOpRHS(0, std::move(LHS));
+}
+
+/*
+ * ParseBinOpRHS - 
+ */
+static std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec, std::unique_ptr<ExprAST> LHS)
+{
+    // If this is a binop, find its precedence.
+    while (true)
+    {
+        if TokPrec = GetTokPrecedence();
+
+        // If this is a binop that binds at least as tightly as the current
+        // binop, consume it, otherwise, we are done.
+        if (TokPrec < ExprPrec)
+            return LHS;
+
+        // We now this is a binop.
+        int BinOp = CurTok;
+        getNextToken();    // Consume the binop.
+
+        // Parse primary expression after binary operator.
+        auto RHS = ParsePrimary();
+        if (!RHS)
+            return nullptr;
+
+        // If BinOp binds less tightly with RHS than the operator after RHS,
+        // let the pending operator take RHS as its LHS.
+        int NextPrec = GetTokenPrecedence();
+        if (TokPrec < NextPrec)
+        {
+            RHS = ParseBinOpRHS(TokPrec + 1, std::move(RHS));
+            if (!RHS)
+                return nullptr;
+        }
+
+        // Merge LHS/RHS.
+        LHS = llvm::make_unique<BinaryExprAST>(BinOp, std::move(LHS), std::move(RHS));
+    }
+}
+
+/*
+ * ParsePrototype - 
+ */
+static std::unique_ptr<ProtypeAST> ParsePrototype()
+{
+    if (CurTok != tok_identifier)
+        return LogErrorP("Expected function name in prototype");
+    
+    std::string FnName = IdentifierStr;
+    getNextToken();
+
+    if (CurTok != '(')
+        return LogErrorP("Expected '(' in prototype");
+
+    // Read the list of arguments.
+    std::vector<std::string> ArgNames;
+    while (getNextToken() == tok_identifier)
+        ArgNames.push_back(IdentifierStr);
+
+    if (CurTok != ')')
+        return LogErrorP("Expected ')' in prototype");
+
+    // Success.
+    getNextToken();   // Eat '('.
+
+    return llvm::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
+}
+
+/*
+ * ParseDefinition - 
+ */
+static std::unique_ptr<FunctionAST> ParseDefinition()
+{
+    getNextToken();    // eat def.
+    auto Proto = ParsePrototype();
+    if (!Proto)
+        return nullptr;
+
+    if (auto E = ParseExpression())
+        return llvm::make_unique<FunctionAST>(std::move(Proto), std::move(E));
+ 
+    return nullptr;
+}
+
+/*
+ * ParseExtern -
+ */
+static std::unique_ptr<PrototypeASP> ParseExtern()
+{
+    getNextToken();    // eat extern.
+    return ParsePrototype();
+}
+
+/*
+ * ParseTopLevelExpr - 
+ */
+static std::unique_ptr<FunctionAST> ParseTopLevelExpr()
+{
+    if (auto E = ParseExpression())
+    {
+        // Make an annonymous proto.
+        auto Proto = llvm::make_unique<PrototypeAST>("", std::vector<std::string>());
+        return llvm::make_unique<FunctionAST>(std::move(Proto), std::move(E));
+    }
+    return nullptr;
+}
+
+/******************************************************************************
+ *                              Top-Level Parsing                             *
+ *****************************************************************************/
+static void HandleDefinition()
+{
+    if (ParseDefinition()) {
+        fprintf(stderr, "Parsed a function definition.\n");
+    } else {
+        // Skip token for error recovery.
+        getNextToken();
+    }
+}
+
+static void HandleExtern()
+{
+    if (ParseExtern()) {
+        fprintf(stderr, "Parsed an extern\n");
+    } else {
+        // Skip token for error recovery.
+        getNextToken();
+    }
+}
+
+static void HandleTopLevelExpression()
+{
+    // Evaluate a top-level expression into an anonymous function.
+    if (ParseTopLevelExpr()) {
+        fprintf(stderr, "Parsed a top-level expr\n");
+    } else {
+        // Skip token for error recovery.
+        getNextToken();
+    }
+}
+
+/// top ::= definition | external | expression | ';'
+static void MainLoop()
+{
+    while (true)
+    {
+        fprintf(stderr, "ready> ");
+        switch (CurTok)
+        {
+            case tok_eof:
+                return;
+            case ';': // ignore top-level semicolons.
+                getNextToken();
+                break;
+            case tok_def:
+                HandleDefinition();
+                break;
+            case tok_extern:
+                HandleExtern();
+                break;
+            default:
+                HandleTopLevelExpression();
+                break;
+        }
+    }
+}
+
+
+
+int main()
+{
+    BinopPrecedence['<'] = 10;
+    BinopPrecedence['+'] = 20;
+    BinopPrecedence['-'] = 30;
+    BinopPrecedence['*'] = 40;
+
+    // Prime the first token.
+    fprintf(stderr, "ready> ");
+    getNextToken();
+    
+    // Run the main "interpreter loop" now.
+    MainLoop();
+    return 0;
+}
