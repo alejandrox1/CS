236d32129fe2c0d182fe297de558b8ea411f3a82
alejandrox1 <alarcj137@gmail.com>
2018-04-17 14:46:01 -0400

    osdev/oswiki/bare-bones/kernel/kernel.c added cross-compiler guards to kernel entrypoint

--- osdev/oswiki/bare-bones/kernel/kernel.c
+++ osdev/oswiki/bare-bones/kernel/kernel.c
@@ -1,5 +1,13 @@
 #include <vga.h>
 
+#if defined(__linux__)
+#error "Cross-compiler is not being used"
+#endif
+
+#if !defined(__i386__)
+#error "This needs to be compiled for a 32-bit ix86 target"
+#endif
+
 void kernel_main(void)
 {
     terminal_initialize();
