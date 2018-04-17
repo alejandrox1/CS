86c71b4a9f3d63caacfeb2e9d7b6a0d62c2b0cc9
alejandrox1 <alarcj137@gmail.com>
2018-04-17 10:31:00 -0400

    osdev/oswiki/bare-bones/lib/string.c strlen implementation

---
+++ osdev/oswiki/bare-bones/lib/string.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stddef.h>
+
+#ifndef __HAVE_ARCH_STRLEN
+/*
+ * strlen - Find the length of a string.
+ * @s: The string o be sized.
+ */
+size_t strlen(const char *s)
+{
+    const char *sc;
+
+    for (sc = s; *sc != '\0'; ++sc)
+        /* Nothing. */;
+    return sc - s;
+}
+#endif
