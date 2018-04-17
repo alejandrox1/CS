156d336b50c33622818c1fc2b49425b591629c0c
alejandrox1 <alarcj137@gmail.com>
2018-04-17 14:45:34 -0400

    osdev/oswiki/bare-bones/drivers/vga.h rm cross-compiler guards from vga api

--- osdev/oswiki/bare-bones/drivers/vga.h
+++ osdev/oswiki/bare-bones/drivers/vga.h
@@ -5,14 +5,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#if defined(__linux__)
-#error "Cross-compiler is not being used"
-#endif
-
-#if !defined(__i386__)
-#error "This needs to be compiled for a 32-bit ix86 target"
-#endif
-
 
 void terminal_initialize(void);
 
