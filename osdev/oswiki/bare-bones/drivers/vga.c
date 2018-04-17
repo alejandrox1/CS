38c3323f8964e6f878b803f2fee548c350fe7edf
alejandrox1 <alarcj137@gmail.com>
2018-04-17 14:07:11 -0400

    osdev/oswiki/bare-bones/drivers/vga.c implemented vga api

--- osdev/oswiki/bare-bones/drivers/vga.c
+++ osdev/oswiki/bare-bones/drivers/vga.c
@@ -1,5 +1,6 @@
 #include <vga.h>
 #include <stddef.h>
+#include <string.h>
 
 static const size_t VGA_WIDTH  = 80;
 static const size_t VGA_HEIGHT = 29;
@@ -33,6 +34,9 @@ uint8_t terminal_color;
 
 static inline uint8_t  vga_entry_color(enum vga_color fg, enum vga_color bg);
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color);
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
+void terminal_putchar(char c);
+void terminal_write(const char *data, size_t size);
 /******************************************************************************
  *                                Public API                                  *
  *****************************************************************************/
@@ -54,7 +58,25 @@ void terminal_initialize(void)
             terminal_buffer[index] = vga_entry(' ', terminal_color);
         }
     }
+}
 
+/*
+ * terminal_setcolor - Accessor for terminal_color global variable.
+ * @color: uint8_t type sets terminal_color.
+ */
+void terminal_setcolor(uint8_t color)
+{
+    terminal_color = color;
+}
+
+/*
+ * terminal_writestring - Write a string to VGA terminal.
+ * @data: (const char *) String to be written.
+ */
+void terminal_writestring(const char *data)
+{
+    terminal_write(data, strlen(data));
+}
 /******************************************************************************
  *                               Private API                                  *
  *****************************************************************************/
@@ -77,4 +99,47 @@ static inline uint8_t vag_entry_color(enum vga_color fg, enum vga_color bg)
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
     return (uint16_t)uc | ((uint16_t)color << 8);
+}
+
+/*
+ * terminal_putentryat - Write character of a given color to VGA terminal. Uses
+ * vga_entry.
+ * @c: (char) character to write to VGA terminal.
+ * @color: (uint8_t) color scheme.
+ * @x: (size_t) VGA position.
+ * @y: (size_t) VGA position.
+ */
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
+{
+    const size_t index = y * VGA_WIDTH + x;
+    terminal_buffer[index] = vga_entry(c, color);
+}
+
+/*
+ * terminal_putchar - Write a character to the next position available on the
+ * terminal.
+ * @c: (char) character to be written.
+ */
+void terminal_putchar(char c)
+{
+    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    if (++terminal_column == VGA_WIDTH)
+    {
+        terminal_column = 0;
+        if (++terminal_row == VGA_HEIGHT)
+            terminal_row = 0;
+    }
+}
+
+/*
+ * terminal_write - Write size number of characters from a string to the VGA
+ * terminal.
+ * @data: (const char *) String to be written.
+ * @size: (size_t) Number of characters to be written.
+ */
+void terminal_write(const char *data, size_t size)
+{
+    size_t i;
+    for (i = 0; i < size; ++i)
+        terminal_putchar(data[i]);
 }
