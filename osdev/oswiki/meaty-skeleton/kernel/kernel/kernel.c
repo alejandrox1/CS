462d6469f7038dabe8ca7e51df75a1cf40d1388e
alejandrox1 <alarcj137@gmail.com>
2018-04-19 15:23:04 -0400

    osdev/oswiki/meaty-skeleton/kernel/kernel/kernel.c added kernel entrypoint

---
+++ osdev/oswiki/meaty-skeleton/kernel/kernel/kernel.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <kernel/tty.h>
+
+#if defined(__linux__)
+#error "Cross-compiler is not being used"
+#endif
+
+#if !defined(__i386__)
+#error "This needs to be compiled for a 32-bit ix86 target"
+#endif
+
+void kernel_main(void)
+{
+    terminal_initialize();
+    terminal_writestring("Hello, kernel world!\n");
+}
