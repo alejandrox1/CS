a67ea4927f0da0d32168197c51694af89af32514
alejandrox1 <alarcj137@gmail.com>
2018-04-19 17:27:48 -0400

    osdev/oswiki/meaty-skeleton/libc/include/string.h added mem-set required methods

--- osdev/oswiki/meaty-skeleton/libc/include/string.h
+++ osdev/oswiki/meaty-skeleton/libc/include/string.h
@@ -1,8 +1,21 @@
 #ifndef _STRING_H
-#define _STRING_H
+#define _STRING_H 1
 
+#include <sys/cdefs.h>
 #include <stddef.h>
 
+#ifdef __cpluplus
+extern "C" {
+#endif
+
+int memcmp(const void* aptr, const void* bptr, size_t size);
+void *memcpy(void* __restrict dstptr, const void* __restrict srcptr, size_t size);
+void *memmove(void* dstptr, const void* srcptr, size_t size);
+void *memset(void* bufptr, int value, size_t size);
 extern size_t strlen(const char *s);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
