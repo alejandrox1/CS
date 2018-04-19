3ceb01670fa1a35c9f0cde11eb170d84afc42c12
alejandrox1 <alarcj137@gmail.com>
2018-04-19 12:13:36 -0400

    llvm/tutorials/00-lexer/main.cpp fixed typos and added an interactive procedure

--- llvm/tutorials/00-lexer/main.cpp
+++ llvm/tutorials/00-lexer/main.cpp
@@ -8,43 +8,38 @@
  * Lexer return tokens [0-255] if it is an unkown character, otherwise one of
  * these for known things.
  */
-enum Token
-{
-    tok_eof = -1;
+enum Token {
+    tok_eof = -1,
 
     // Commands.
-    tok_def    = -2;
-    tok_extern = -3;
+    tok_def = -2,
+    tok_extern = -3,
 
     // Primary.
-    tok_identifier = -4;
-    tok_number     = -5;
+    tok_identifier = -4,
+    tok_number = -5,
 };
 
-
-static std::string IdentifierStr;    // Filled in if tok_identifier.
-static double      NumVal;           // Filled in if tok_number.
-
+static std::string IdentifierStr; // Filled in if tok_identifier.
+static double NumVal;             // Filled in if tok_number.
 
 /*
  * gettok (lexer) returns the next token from standard input.
  */
-static int gettok()
-{
-    static int LastCHar = ' ';
+static int gettok() {
+    static int LastChar = ' ';
 
     // Skip nay white spaces.
     while (isspace(LastChar))
         LastChar = getchar();
 
     // Identifier: [a-zA-Z][a-zA-Z0-9]*
-    if (isalpha(LastChar))
-    {
+    if (isalpha(LastChar)) {
         IdentifierStr = LastChar;
         while (isalnum((LastChar = getchar())))
-            IndeitiferStr += LastChar;
+            IdentifierStr += LastChar;
 
-        if (IdentiferStr == "def")
+        if (IdentifierStr == "def")
             return tok_def;
         if (IdentifierStr == "extern")
             return tok_extern;
@@ -52,26 +47,24 @@ static int gettok()
     }
 
     // Number: [0-9].+
-    if (isdigit(LastChar) || LastChar == '.')
-    {
+    if (isdigit(LastChar) || LastChar == '.') {
         std::string NumStr;
         do {
             NumStr += LastChar;
-            LastCHar += getchar();
-        } while(isdigit(LastChar) || LastChar == '.');
+            LastChar += getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
 
         NumVal = strtod(NumStr.c_str(), nullptr);
         return tok_number;
     }
 
-    if (LastChar == '#')
-    {
+    if (LastChar == '#') {
         // Comment until end of line.
         do
             LastChar = getchar();
         while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
 
-        if (LastCHar != EOF)
+        if (LastChar != EOF)
             return gettok();
     }
 
@@ -81,6 +74,52 @@ static int gettok()
 
     // [Operatores] Otherwise, return character as its ASCII value.
     int ThisChar = LastChar;
-    LastCHar = getchar();
+    LastChar = getchar();
     return ThisChar;
+}
+
+static int CurTok;
+static int getNextToken() { return CurTok = gettok(); }
+
+static void MainLoop() {
+    while (true) {
+        fprintf(stderr, "ready> ");
+        switch (CurTok) {
+        case tok_eof:
+            fprintf(stderr, "\tEOF\n");
+            return;
+        case ';': // ignore top-level semicolons.
+            fprintf(stderr, "\tTop-level semicolon.\n");
+            getNextToken();
+            break;
+        case tok_def:
+            getNextToken(); // eat def
+            fprintf(stderr, "Parsed a function definition.\n");
+            fprintf(stderr, "\tFunc def: %s.\n", IdentifierStr.c_str());
+            break;
+        case tok_extern:
+            fprintf(stderr, "\tExtern: %s.\n", IdentifierStr.c_str());
+            getNextToken();
+            break;
+        case tok_identifier:
+            fprintf(stderr, "\tIdetifier: %s.\n", IdentifierStr.c_str());
+            getNextToken();
+            break;
+        case tok_number:
+            fprintf(stderr, "\tNumber: %f.\n", NumVal);
+            getNextToken();
+            break;
+        default:
+            getNextToken();
+            break;
+        }
+    }
+}
+
+int main() {
+    fprintf(stderr, "ready> ");
+    getNextToken();
+    MainLoop();
+
+    return 0;
 }
