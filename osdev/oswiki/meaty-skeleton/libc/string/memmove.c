3a2ec3cebaed1eec34f9623b5cda33047fcbec4a
alejandrox1 <alarcj137@gmail.com>
2018-04-20 11:57:33 -0400

    osdev/oswiki/meaty-skeleton/libc/string/memmove.c style fix to memmove

--- osdev/oswiki/meaty-skeleton/libc/string/memmove.c
+++ osdev/oswiki/meaty-skeleton/libc/string/memmove.c
@@ -5,15 +5,14 @@ void *memmove(void* dstptr, const void* srcptr, size_t size)
     unsigned char* dst = (unsigned char*)dstptr;
     const unsigned char* src = (const unsigned char*)srcptr;
     
-    size_t i;
     if (dst < src)
     {
-        for (i = 0; i < size; ++i)
+        for (size_t i = 0; i < size; ++i)
             dst[i] = src[i];
     }
     else
     {
-        for (i = size; i != 0; --i)
+        for (size_t i = size; i != 0; --i)
             dst[i-1] = src[i-1]
     }
     return dstptr;
