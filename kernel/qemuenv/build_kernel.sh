b31ea9b0710b2b99b038de950910034504f73c4e
alejandrox1 <alarcj137@gmail.com>
2018-04-27 15:31:45 -0400

    kernel/qemuenv/build_kernel.sh specified a mirror to download a specififc kernel version

--- kernel/qemuenv/build_kernel.sh
+++ kernel/qemuenv/build_kernel.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# If interested in a particular version
+# curl -C - -O https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/linux-4.16.5.tar.gz
+# or wget -c https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/linux-4.16.5.tar.gz
+
 git clone --depth=1 git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git && \
     cd linux && \
     make x86_64_defconfig && \
