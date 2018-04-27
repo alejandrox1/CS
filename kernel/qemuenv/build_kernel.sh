a711c849936eff383bce65cbd16881ba9ccc1a80
alejandrox1 <alarcj137@gmail.com>
2018-04-27 15:28:56 -0400

    kernel/qemuenv/build_kernel.sh script to build latest kernel version

---
+++ kernel/qemuenv/build_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+git clone --depth=1 git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git && \
+    cd linux && \
+    make x86_64_defconfig && \
+    make kvmconfig && \
+    make -j 8
+
