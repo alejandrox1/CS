7c37368eb3df979aadace73f054608a47a6f097a
alejandrox1 <alarcj137@gmail.com>
2018-04-27 15:24:28 -0400

    kernel/qemuenv/build_fs.sh setting a proper and eficient kernel dev env

---
+++ kernel/qemuenv/build_fs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+ARCH=amd64
+DIST=xenial
+IMG=vmroot_${DIST}_${ARCH}.img
+DIR=mount_point
+
+qemu-img create $IMG 1g
+mkfs.ext2 $IMG
+
+
+mkdir -p $DIR
+sudo mount -o loop $IMG $DIR
+sudo debootstrap --variant=buildd --arch ${ARCH} ${DIST} ${DIR}
+sudo umount ${DIR}
+
