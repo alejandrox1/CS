a314660e7f4822338d2e9204dc1fc5d41639a6c4
alejandrox1 <alarcj137@gmail.com>
2018-04-27 16:25:50 -0400

    kernel/qemuenv/run-qemu.sh best way to run qemu

--- kernel/qemuenv/run-qemu.sh
+++ kernel/qemuenv/run-qemu.sh
@@ -4,4 +4,6 @@ set -e
 
 qemu-system-x86_64 -kernel linux/arch/x86/boot/bzImage \
     -hda vmroot_xenial_amd64.img \
-    -append "root=/dev/sda single"
+    -append "root=/dev/sda console=ttyS0" \
+    --enable-kvm \
+    -serial stdio
