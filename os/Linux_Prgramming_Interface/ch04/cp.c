b5d412f35845b5f44b2d3689c36ffc4c6270e219
alejandrox1 <alarcj137@gmail.com>
2018-05-01 15:39:49 -0400

    os/Linux_Prgramming_Interface/ch04/cp.c untested impl of cp

--- os/Linux_Prgramming_Interface/ch04/cp.c
+++ os/Linux_Prgramming_Interface/ch04/cp.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "error_functions.h"
 
 #ifndef BUFF_SIZE
@@ -20,10 +21,10 @@
 
 int main(int argc, char *argv[])
 {
-    int inputFd; //, outputFd, openFlags;
-    //mode_t filePerms;
-    //ssize_t numRead;
-    //char buf[BUFF_SIZE];
+    int inputFd, outputFd, openFlags;
+    mode_t filePerms;
+    ssize_t numRead;
+    char buf[BUFF_SIZE];
 
     if (argc != 3 || strcmp(argv[1], "--help") == 0)
         usageError("%s old-file new-file\n", argv[0]);
@@ -33,5 +34,24 @@ int main(int argc, char *argv[])
     if (inputFd == -1)
         errExit("opening file %s", argv[1]);
 
-    return 0;
+    openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+    filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH; /* rw-rw-rw- */
+    outputFd  = open(argv[2], openFlags, filePerms);
+    if (outputFd == -1)
+        errExit("opening file %s", argv[2]);
+
+    /* Transfer data until we encounter end of input or an error. */
+    while ((numRead = read(inputFd, buf, BUFF_SIZE)) > 0)
+        if (write(outputFd, buf, numRead) != numRead)
+            fatal("couldn't write whole buffer");
+
+    if (numRead == -1)
+        errExit("read");
+
+    if (close(inputFd) == -1)
+        errExit("close input");
+    if (close(outputFd) == -1)
+        errExit("close output");
+
+    exit(EXIT_SUCCESS);
 }
