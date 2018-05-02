1186468f34b07ffca5fb92aa1c4320535f3d8f67
alejandrox1 <alarcj137@gmail.com>
2018-05-02 14:12:40 -0400

    os/Linux_Prgramming_Interface/ch04/get_num.c starter get_num implementation

---
+++ os/Linux_Prgramming_Interface/ch04/get_num.c
@@ -0,0 +1,75 @@
+/*
+ * get_num.c 
+ *
+ * Functions to process numerica command-line arguments.
+ *
+ * Taken from the Linux Programming Interface.
+ */
+#include <errno.h>
+#include <stdlib.h>
+
+static long getNum(const char* fname, const char* arg, int flags, const char* name);
+/******************************************************************************
+ *                             Public API                                     *
+ *****************************************************************************/
+/**
+ * getLong - Convert a numeric command-line argument into a long integer.
+ * See getNUm() for a description of the arguments to this function.
+ */
+long getLong(const char* arg, int flags, const char* name)
+{
+    return getNum("getLong", arg, flags, name);
+}
+
+/******************************************************************************
+ *                             Private API                                    *
+ *****************************************************************************/
+/**
+ * gnFail - Print a diagnostic message that contains a function name, value of
+ * a command-line argument, the name of that command-line argument, and a
+ * disagnostic message.
+ *
+ * @fname: Function name.
+ * @msg: Diagnostic message.
+ * @arg: Command-line argument.
+ * @name: Name of command-line argument.
+ */
+/**
+ * getNum - Convert a numeric command-line argument ('arg') into a long integer
+ * and return it.
+ * 'fname' and 'name' are used to print a diagnostic message in case an error
+ * is detected when processing 'arg'.
+ *
+ * @fname: Name of caller.
+ * @arg: Command-line argument.
+ * @flags: Bit mask of flags controlling how the conversion is done and what
+ * diagnostic checks are performed on the numeric result.
+ * @name: Name associated with command line argument 'arg'.
+ */
+static long getNum(const char* fname, const char* arg, int flags, const char* name)
+{
+    long rest;
+    char* endptr;
+    int base;
+
+    if (arg == NULL || *arg == '\0')
+        gnFail(fname, "null or empty string", arg, name);
+
+    base = (flags & GN_ANY_BASE) ? 0 : (flags & GN_BASE_8) ? 8 : (flags & GN_BASE_16) ? 16 : 10;
+
+    errno = 0;
+    res = strtol(argv, &endptr, base);
+    if (errno != 0)
+        gnFail(fname, "strtol() failed", arg, name);
+
+    if (*endptr != '\0')
+        gnFail(fname, "nonumeric characters", arg, name);
+
+    if ((flags & GN_NONNGE) && res < 0)
+        gnFail(fname, "negative value not allowed", arg, name);
+
+    if ((flags & GN_GT_0) && res <= 0)
+        gnFail(fname, "value must be > 0", arg, name);
+
+    return res;
+}
