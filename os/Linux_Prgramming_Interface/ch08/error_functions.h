188219f10951c91e6691c7431b108302fd7ae479
alejandrox1 <alarcj137@gmail.com>
2018-05-05 12:36:04 -0400

    os/Linux_Prgramming_Interface/ch08/error_functions.c error functions api

---
+++ os/Linux_Prgramming_Interface/ch08/error_functions.h
@@ -0,0 +1,20 @@
+/*
+ * error_functions.h
+ *
+ * Taken from the Linux Programming Interface.
+ */
+#ifndef ERROR_FUNCTIONS_H
+#define ERROR_FUNCTIONS_H
+
+#ifdef __GNUC__
+#define NORETURN __attribute__((__noreturn__))
+#else
+#define NORETURN
+#endif
+
+void cmdLineErr(const char* format, ...) NORETURN;
+void errExit(const char* format, ...) NORETURN;
+void fatal(const char* format, ...) NORETURN;
+void usageError(const char* format, ...) NORETURN;
+
+#endif
