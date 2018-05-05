4e9c59b6a8aa984a0bdbfe02f285ead0275657fa
alejandrox1 <alarcj137@gmail.com>
2018-05-05 12:36:38 -0400

    os/Linux_Prgramming_Interface/ch08/error_functions.h Making Boolean type public

--- os/Linux_Prgramming_Interface/ch08/error_functions.h
+++ os/Linux_Prgramming_Interface/ch08/error_functions.h
@@ -6,6 +6,16 @@
 #ifndef ERROR_FUNCTIONS_H
 #define ERROR_FUNCTIONS_H
 
+#ifdef TRUE                                                                     
+#undef TRUE                                                                     
+#endif                                                                          
+                                                                                
+#ifdef FALSE                                                                    
+#undef FALSE                                                                    
+#endif                                                                          
+                                                                                
+typedef enum { FALSE, TRUE } Boolean; 
+
 #ifdef __GNUC__
 #define NORETURN __attribute__((__noreturn__))
 #else
