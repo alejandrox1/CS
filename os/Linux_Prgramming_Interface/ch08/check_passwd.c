784fa171d02761e52ca5de1a182e0d0f8d226b7d
alejandrox1 <alarcj137@gmail.com>
2018-05-04 20:25:54 -0400

    os/Linux_Prgramming_Interface/ch08/check_passwd.c add specs for program

---
+++ os/Linux_Prgramming_Interface/ch08/check_passwd.c
@@ -0,0 +1,8 @@
+/*
+ * check_passwd.c
+ *
+ * Read a user name and password and check if these are valid.
+ * This program assumes use os shadow passwords.
+ *
+ * Taken from The Linux programming Interface.
+ */
