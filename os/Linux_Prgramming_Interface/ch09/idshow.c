d43080cf17a466e08376ba3b30bb231513cadc33
alejandrox1 <alarcj137@gmail.com>
2018-05-07 15:24:18 -0400

    os/Linux_Prgramming_Interface/ch09/idshow.c added specs for idshow.c

---
+++ os/Linux_Prgramming_Interface/ch09/idshow.c
@@ -0,0 +1,10 @@
+/*
+ * idshow.c
+ *
+ * Display all user and group identifiers associated with a process.
+ *
+ * Note: This program uses Linux-specific calls and the Linux-specific
+ * file-system user and group IDs.
+ *
+ * Taken from The Linux Prgramming Interface.
+ */
