5d3b78d1d1961108fa78f46d7db7fd27c46d9db2
alejandrox1 <alarcj137@gmail.com>
2018-05-07 16:02:52 -0400

    os/Linux_Prgramming_Interface/ch09/ugid_functions.h added ugid_functions api declarations

---
+++ os/Linux_Prgramming_Interface/ch09/ugid_functions.h
@@ -0,0 +1,11 @@
+/*                                                                              
+ * ugid_functions.h                                                             
+ *                                                                              
+ * Taken from The Linux Programming Interface.                                  
+ */                                                                             
+#ifndef UGID_FUNCTIONS_H                                                        
+#define UGID_FUNCTIONS_H                                                        
+                                                                                
+char* userNameFromId(uid_t uid);                                                 
+                                                                                
+#endif 
