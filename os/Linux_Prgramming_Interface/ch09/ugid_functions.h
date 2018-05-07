4c89c00ff8a0e58ad89232e1f63d2f8327f5f2b2
alejandrox1 <alarcj137@gmail.com>
2018-05-07 19:01:38 -0400

    os/Linux_Prgramming_Interface/ch09/ugid_functions.h add includes for gid_t/uid_t

--- os/Linux_Prgramming_Interface/ch09/ugid_functions.h
+++ os/Linux_Prgramming_Interface/ch09/ugid_functions.h
@@ -5,7 +5,12 @@
  */                                                                             
 #ifndef UGID_FUNCTIONS_H                                                        
 #define UGID_FUNCTIONS_H                                                        
-                                                                                
+
+#include <pwd.h>
+#include <grp.h>
+
+char* groupNameFromId(gid_t gid);
+
 char* userNameFromId(uid_t uid);                                                 
                                                                                 
 #endif 
