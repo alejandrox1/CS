8d740cd5e13667454a58242de9f778a976402e57
alejandrox1 <alarcj137@gmail.com>
2018-05-09 15:34:40 -0400

    os/Linux_Prgramming_Interface/ch10-time/strtime.c untested version of strtime script

--- os/Linux_Prgramming_Interface/ch10-time/strtime.c
+++ os/Linux_Prgramming_Interface/ch10-time/strtime.c
@@ -14,3 +14,45 @@
  *
  * Taken from The Linux Programming Interface.
  */
+#if ! define(__sun)
+# ifndef _XOPEN_SOURCE
+# define _XOPEN_SOURCE
+# endif
+#endif
+#include <locale.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "error_functions.h"
+
+#define SBUF_SIZE 1000
+
+int main(int argc, char* argv[])
+{
+    struct tm tm;
+    char sbuf[SBUF_SIZE];
+    char* ofmt;
+
+    if (argc < 3 || strcmp(argv[1], "--help") == 0)
+        usgaeError("%s input-date-time in-format [out-format]\n", argv[0]);
+
+    if (setlocale(LC_ALL, "") == NULL)
+        errExit("setlocale");
+
+    memset(&tm, sizeof(struct tm));
+    if (strptime(argv[1], argv[2], &tm) == NULL)
+        fatal("strptime");
+
+    /* Not set by strptime(); tells mktime() to determine if DST is in effect. */
+    tm.tm_isdt = -1;
+    
+    printf("calendar time (seconds since Epoch): %ld\n", (long)mktime(&tm));
+
+    ofmt = (argc > 3) ? argv[3]: "%H:%M:%S %A, %d %B %Y %Z";
+    if (strftime(sbuf, SBUF_SIZE, ofmt, &tm) == 0)
+        fatal("strftime returned 0");
+    printf("strftime() returned: %s\n", sbuf);
+
+    exit(EXIT_SUCCESS);
+}
