bc22c83ba85356a4e5dff6a56566f9fa4f35ace7
alejandrox1 <alarcj137@gmail.com>
2018-05-25 14:29:54 -0500

    HPP/KNC/ch02-test_drive/build.sh build.sh uses skeleton.sh template

--- HPP/KNC/ch02-test_drive/build.sh
+++ HPP/KNC/ch02-test_drive/build.sh
@@ -1,24 +1,58 @@
 #!/bin/bash
+#-
+#-                          :-) Build KNC Examples (-:
+#-
+#- Build script for a variety of sample applications to test drive systems with
+#- HPC-type workloads.
+#-
+##
+## Usage:
+##
+##      ./build.sh [-v|-h] [-i|--input INPUT_FILE]
+##
+##      -v|--version print to stdout any information related to this script.
+##
+##      -h|--help print to stdout any help information included in the header
+##                of the script.
+##
+##      -no-opt normal compilation (no autovectorization enabled).
+##
 set -e
+set -o pipefail
+export reset="\e[0m"
+export red="\e[1;31m"
+export green="\e[32m"
 
-export NO_OPTIMIZATION="false"
+NO_OPTIMIZATION="false"
 
-# Process command-line arguments.
-while [[ $# > 0 ]]; do
-    key="$1"
+# Parse command line arguments.
+while [[ "$#" > 0 ]]; do
+    arg="$1"
 
-    case $key in
+    case "${arg}" in
+        -v|--version)
+            echo "$(grep "^#-" ${BASH_SOURCE[0]} | cut -c 4-)"
+            exit 0
+            ;;
+        -h|--help)
+            echo "$(grep "^##" ${BASH_SOURCE[0]} | cut -c 4-)"
+            exit 0
+            ;;
         -no-opt)
             NO_OPTIMIZATION="true"
-            shift
             ;;
         *)
-            echo "Unkown option."
+            ( >&2 echo "Unknown command-line option: '${arg}'.")
             exit 1
             ;;
     esac
+    shift
 done
+
+
+if [ "${NO_OPTIMIZATION}" == "true" ]; then post="out"; fi
+echo -e "${red}Proceding with${post} auto-vectorization...${reset}"
 
 . ./config.sh
 
-$MAKE clean && $MAKE 
+$MAKE clean && $MAKE
