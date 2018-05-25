a2294c5a8be3b20ae62dc9263b76999789b544ed
alejandrox1 <alarcj137@gmail.com>
2018-05-24 23:56:34 -0500

    HPP/KNC/ch02-test_drive/build.sh parse command-line arguments to enable/disable optimization flags for compilation

--- HPP/KNC/ch02-test_drive/build.sh
+++ HPP/KNC/ch02-test_drive/build.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 set -e
+
+export NO_OPTIMIZATION="false"
+
+# Process command-line arguments.
+while [[ $# > 0 ]]; do
+    key="$1"
+
+    case $key in
+        -no-opt)
+            NO_OPTIMIZATION="true"
+            shift
+            ;;
+        *)
+            echo "Unkown option."
+            exit 1
+            ;;
+    esac
+done
+
 . ./config.sh
 
-$MAKE
+$MAKE clean && $MAKE 
