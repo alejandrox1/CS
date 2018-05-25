027c6dc0dcfcfed6987b7493c281c5cf2bbdbe70
alejandrox1 <alarcj137@gmail.com>
2018-05-25 14:29:34 -0500

    HPP/KNC/ch02-test_drive/config.sh cleaning up script

--- HPP/KNC/ch02-test_drive/config.sh
+++ HPP/KNC/ch02-test_drive/config.sh
@@ -5,19 +5,19 @@ export MAKE=${MAKE:-make}
 
 if command -v icc > /dev/null 2>&1; then    # Intel compilation configuration.
     export CC=icc
-    if [ "${NO_OPTIMIZATION}" == "false" ]; then
-        export CFLAGS='-mmic -vec-report=3 -O3'
-    else
+    if [ "${NO_OPTIMIZATION}" == "true" ]; then
         export CFLAGS='-no-vec -no-simd'
+    else
+        export CFLAGS='-mmic -vec-report=3 -O3'
     fi
 elif command -v gcc > /dev/null 2>&1; then  # GCC compilation configuration.
     export CC=gcc
-    if [ "${NO_OPTIMIZATION}" == "false" ]; then
-        export CFLAGS='-Wall -Wextra -Ofast -mavx -mfma -std=gnu11'
-    else
+    if [ "${NO_OPTIMIZATION}" == "true" ]; then
         export CFLAGS='-Wall -Wextra -std=gnu11'
+    else
+        export CFLAGS='-Wall -Wextra -Ofast -mavx -mfma -std=gnu11'
     fi
 else
-    echo Neither the Intel compilers nor GCC are available...
+    echo -e "${red}Neither the Intel compilers nor GCC are available...${reset}"
     exit 1
 fi
