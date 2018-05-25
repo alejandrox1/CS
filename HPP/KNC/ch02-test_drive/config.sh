60de6ccd52af5d3cb6e41d804b282a37ceb00e59
alejandrox1 <alarcj137@gmail.com>
2018-05-24 23:57:16 -0500

    HPP/KNC/ch02-test_drive/config.sh set up compilation environment

--- HPP/KNC/ch02-test_drive/config.sh
+++ HPP/KNC/ch02-test_drive/config.sh
@@ -2,5 +2,22 @@
 set -e
 
 export MAKE=${MAKE:-make}
-export CC=gcc
-export CFLAGS='-Wall -Wextra -Ofast -mavx -mfma -std=gnu11'
+
+if command -v icc > /dev/null 2>&1; then    # Intel compilation configuration.
+    export CC=icc
+    if [ "${NO_OPTIMIZATION}" == "false" ]; then
+        export CFLAGS='-mmic -vec-report=3 -O3'
+    else
+        export CFLAGS='-no-vec -no-simd'
+    fi
+elif command -v gcc > /dev/null 2>&1; then  # GCC compilation configuration.
+    export CC=gcc
+    if [ "${NO_OPTIMIZATION}" == "false" ]; then
+        export CFLAGS='-Wall -Wextra -Ofast -mavx -mfma -std=gnu11'
+    else
+        export CFLAGS='-Wall -Wextra -std=gnu11'
+    fi
+else
+    echo Neither the Intel compilers nor GCC are available...
+    exit 1
+fi
