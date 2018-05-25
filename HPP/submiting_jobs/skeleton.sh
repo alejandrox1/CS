572894b92b1f1becdd95f265572bcbf99de579b2
alejandrox1 <alarcj137@gmail.com>
2018-05-25 12:30:01 -0500

    HPP/submiting_jobs/skeleton.sh read arguments from user-specified json file

--- HPP/submiting_jobs/skeleton.sh
+++ HPP/submiting_jobs/skeleton.sh
@@ -15,6 +15,13 @@
 ##      -h print to stdout any help information included in the header of the
 ##         script.
 ##
+set -e
+set -o pipefail
+reset="\e[0m";
+red="\e[1;31m";
+
+# Input parameters
+INPUT_FILE="parameters.json"
 
 # Parse command line arguments.
 while [[ "$#" > 0 ]]; do
@@ -29,9 +36,20 @@ while [[ "$#" > 0 ]]; do
             echo "$(grep "^##" ${BASH_SOURCE[0]} | cut -c 4-)"
             exit 0
             ;;
+        -i|--input)
+            INPUT_FILE="$2"
+            shift
+            ;;
         *)
-            ( >&2 echo "Unknown command-line option.")
+            ( >&2 echo "Unknown command-line option: '${arg}'.")
             exit 1
             ;;
     esac
+    shift
 done
+
+echo -e "${red}Proceding with input parameter file ${INPUT_FILE}${reset}"
+REQUESTED_NODES=`jq .job_parameters.nodes "${INPUT_FILE}"`
+REQUESTED_CORES=`jq .job_parameters.cores "${INPUT_FILE}"`
+echo Number of nodes requested: ${REQUESTED_NODES}"
+echo Number of cores requested: ${REQUESTED_CORES}"
