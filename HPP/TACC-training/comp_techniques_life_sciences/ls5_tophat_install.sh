620052d65cabaa708fb9d4dba070a354623ba695
alejandrox1 <alarcj137@gmail.com>
2018-05-28 17:27:55 -0500

    HPP/TACC-training/comp_techniques_life_sciences/ls5_tophat_install.sh cleanup tophat src before retrying

--- HPP/TACC-training/comp_techniques_life_sciences/ls5_tophat_install.sh
+++ HPP/TACC-training/comp_techniques_life_sciences/ls5_tophat_install.sh
@@ -25,6 +25,7 @@ export LDFLAGS="-xAVX -axCORE-AVX2"
  
 # Create clone and get specific version from tags.
 (
+    rm -rf "${TOPHAT_PATH}" && \
     mkdir -p "${APPS_SRC}" "${TOPHAT_PATH}" && cd "${APPS_SRC}" && \
     wget --no-check-certificate "https://ccb.jhu.edu/software/tophat/downloads/${TOPHAT_PKG_VER}.tar.gz" && \
         tar -xvzf "${TOPHAT_PKG_VER}.tar.gz" && \
