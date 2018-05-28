e527ee83837cda1412bb317bcdb3c42607303917
alejandrox1 <alarcj137@gmail.com>
2018-05-28 17:22:10 -0500

    HPP/TACC-training/comp_techniques_life_sciences/ls5_tophat_install.sh cloning repo leads to missing dependencies, install from tarball instead

--- HPP/TACC-training/comp_techniques_life_sciences/ls5_tophat_install.sh
+++ HPP/TACC-training/comp_techniques_life_sciences/ls5_tophat_install.sh
@@ -13,6 +13,7 @@ export APPS_SRC="${WORK}/apps"
 export TOPHAT_SRC="${APPS_SRC}/tophat"                                             
 export TOPHAT_VERSION="v2.1.1"
 export TOPHAT_PATH="${TOPHAT_SRC}/${TOPHAT_VERSION}"
+export TOPHAT_PKG_VER="tophat-2.1.1"
 # Use Intel compilers to install tophat.
 export CC=`which icc`
 export CXX=`which icpc`
@@ -21,20 +22,29 @@ export CFLAGS="-xAVX -axCORE-AVX2"
 export CXXFLAGS="-xAVX -axCORE-AVX2"
 export LDFLAGS="-xAVX -axCORE-AVX2"
 
-
+ 
 # Create clone and get specific version from tags.
-(    
+(
     mkdir -p "${APPS_SRC}" && cd "${APPS_SRC}" && \
-    git clone https://github.com/infphilo/tophat "${TOPHAT_PATH}" && \
-    cd "${TOPHAT_PATH}" && \
-    git fetch origin --tags --prune && \
-    git checkout "${TOPHAT_VERSION}"
+    wget --no-check-certificate "https://ccb.jhu.edu/software/tophat/downloads/${TOPHAT_PKG_VER}.tar.gz" && \
+        tar -xvzf "${TOPHAT_PKG_VER}.tar.gz" && \
+        mv "${TOPHAT_PKG_VER}" "${TOPHAT_PATH}" && \
 )
+# The bellow approach returns the following error:
+# configure: error: cannot find sources (config.h.in) in . or ..
+#(    
+#    mkdir -p "${APPS_SRC}" && cd "${APPS_SRC}" && \
+#    git clone https://github.com/infphilo/tophat "${TOPHAT_PATH}" && \
+#    cd "${TOPHAT_PATH}" && \
+#    git fetch origin --tags --prune && \
+#    git checkout "${TOPHAT_VERSION}" && \
+#    aclocal && autoconf && automake --add-missing
+#)
+
 
 # Configure tophat - take advantage of the Intel processors.
 (
     cd "${TOPHAT_PATH}" && \
-        aclocal && autoconf && \
         ./configure --prefix="${TOPHAT_PATH}" \
         --with-boost="${TACC_BOOST_DIR}" \
         --enable-intel64 && \
