4849d42510d99a4c7c6b8b6b71418a456359b1ee
alejandrox1 <alarcj137@gmail.com>
2018-05-29 09:37:47 -0500

    HPP/TACC-training/comp_techniques_life_sciences/working_with_modules/2.1.1.lua sample module file

---
+++ HPP/TACC-training/comp_techniques_life_sciences/working_with_modules/2.1.1.lua
@@ -0,0 +1,21 @@
+help (
+[[
+This module is for a locally-installed version of tophat.
+
+Version 2.1.1
+
+]])
+
+whatis("Name: tophat")
+whatis("Version: 2.1.1")
+whatis("Category: computational biology, genomics")
+whatis("Keywords: Biology, Genomics, Alignment, Sequencing")
+whatis("URL: https://ccb.jhu.edu/software/tophat/tutorial.shtml")
+whatis("Description: A spliced read mapper for RNA-seq")
+
+setenv("TACC_TOPHAT_DIR", "/work/03439/wallen/lonestar/apps/tophat/2.1.1")
+prepend_path("PATH",      "/work/03439/wallen/lonestar/apps/tophat/2.1.1/bin")
+
+prereq("bowtie/2.2.6")
+prereq("boost/1.59")
+prereq("intel/16.0.1")
