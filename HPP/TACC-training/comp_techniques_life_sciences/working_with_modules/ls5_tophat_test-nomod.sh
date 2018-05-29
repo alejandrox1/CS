43da1e45303c19bf0c98cccfee8860f3f5f4ece9
alejandrox1 <alarcj137@gmail.com>
2018-05-29 16:12:41 -0500

    HPP/TACC-training/comp_techniques_life_sciences/working_with_modules/ls5_tophat_test-nomod.sh test tophat instllation

---
+++ HPP/TACC-training/comp_techniques_life_sciences/working_with_modules/ls5_tophat_test-nomod.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#SBATCH -A my_project
+#SBATCH -p normal
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 10:00
+#SBATCH -J tophat_test
+#SBATCH -o slurm.%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alarcj137@gmail.com
+set -e                                                                          
+set -o pipefail                                                                 
+set -o posix
+
+export PATH=$PATH:/work/05692/jochoa/lonestar/apps/tophat/v2.1.1/bin
+
+export APPS_SRC="${WORK}/apps"
+export TOPHAT_TEST="${APPS_SRC}/tophat-test"
+
+mkdir -p "${TOPHAT_TEST}" && \
+    wget --no-check-certificate https://ccb.jhu.edu/software/tophat/downloads/test_data.tar.gz && \
+    tar -xvzf test_data.tar.gz && \
+    cd test_data && \
+    tophat -r 20 test_ref reads_1.fq reads_2.fq
