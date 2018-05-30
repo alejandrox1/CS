67d5d4ed93ca2b8cba2bff38fb5016194630cd80
alejandrox1 <alarcj137@gmail.com>
2018-05-30 14:54:36 -0500

    mpi_stuff/docker-cluster/cluster.sh added generate_ssh_keys method

--- mpi_stuff/docker-cluster/cluster.sh
+++ mpi_stuff/docker-cluster/cluster.sh
@@ -13,15 +13,29 @@ set -o pipefail
 . ./config.sh
 
 down_all() {
-    echo -e "${blue}${ASCII_ART_HEADER}${reset}"
-    echo -e "${green}$docker-compose down registry${reset}"
+    echo -e "${green}docker-compose down${reset}"
 
-    docker-compose down registry
+    docker-compose down
 }
 
 up_registry() {
-    echo -e "${blue}${ASCII_ART_HEADER}${reset}" 
-    echo -e "${green}$docker-compose up -d registry${reset}"
+    echo -e "${green}docker-compose up -d registry${reset}"
 
     docker-compose up -d registry
 }
+
+generate_ssh_keys() {
+    if [ -f ssh/id_rsa ] && [ -f ssh/id_rsa.pub ]; then
+        return 0
+    fi
+
+    echo -e "${green}Generating SSH keys...${reset}"
+    
+    mkdir -p ssh
+    ssh-keygen -f ssh/id_rsa -t rsa -N ''
+}
+
+echo -e "${blue}${ASCII_ART_HEADER}${reset}"
+down_all
+up_registry
+generate_ssh_keys
