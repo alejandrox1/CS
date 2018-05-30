defcf501f21fedd2a0f484cb67b434379d198b67
alejandrox1 <alarcj137@gmail.com>
2018-05-30 15:12:57 -0500

    mpi_stuff/docker-cluster/cluster.sh added build_and_push_images method

--- mpi_stuff/docker-cluster/cluster.sh
+++ mpi_stuff/docker-cluster/cluster.sh
@@ -14,13 +14,11 @@ set -o pipefail
 
 down_all() {
     echo -e "${green}docker-compose down${reset}"
-
     docker-compose down
 }
 
 up_registry() {
     echo -e "${green}docker-compose up -d registry${reset}"
-
     docker-compose up -d registry
 }
 
@@ -28,14 +26,21 @@ generate_ssh_keys() {
     if [ -f ssh/id_rsa ] && [ -f ssh/id_rsa.pub ]; then
         return 0
     fi
-
     echo -e "${green}Generating SSH keys...${reset}"
-    
     mkdir -p ssh
     ssh-keygen -f ssh/id_rsa -t rsa -N ''
+}
+
+build_and_push_images() {
+    echo -e "${green}docker build -t ${CLUSTER_REGISTRY_ADDR}:${CLUSTER_REGISTRY_PORT}/${IMAGE_NAME} .${reset}"
+    docker build -t "${CLUSTER_REGISTRY_ADDR}:${CLUSTER_REGISTRY_PORT}/${IMAGE_NAME}" .
+
+    echo -e "${green}docker push ${CLUSTER_REGISTRY_ADDR}:${CLUSTER_REGISTRY_PORT}/${IMAGE_NAME}${reset}"
+    docker push "${CLUSTER_REGISTRY_ADDR}:${CLUSTER_REGISTRY_PORT}/${IMAGE_NAME}"
 }
 
 echo -e "${blue}${ASCII_ART_HEADER}${reset}"
 down_all
 up_registry
 generate_ssh_keys
+build_and_push_images
