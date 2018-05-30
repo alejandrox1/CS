bab77f29bb29d72206dd36f6d9071878583209b0
alejandrox1 <alarcj137@gmail.com>
2018-05-30 15:15:03 -0500

    mpi_stuff/docker-cluster/config.sh define CLUSTER_REGISTRY for quicker ref

--- mpi_stuff/docker-cluster/config.sh
+++ mpi_stuff/docker-cluster/config.sh
@@ -2,5 +2,6 @@
 
 export CLUSTER_REGISTRY_ADDR=localhost                                                 
 export CLUSTER_REGISTRY_PORT=5000                                                      
+export CLUSTER_REGISTRY="${CLUSTER_REGISTRY_ADDR}:${CLUSTER_REGISTRY_PORT}"
 export CLUSTER_SSH_PORT=2222                                                                   
 export IMAGE_NAME=mpi 
