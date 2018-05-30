ae595461b175924226cbd76a57b733e4a3911bc6
alejandrox1 <alarcj137@gmail.com>
2018-05-30 16:21:55 -0500

    mpi_stuff/docker-cluster/utils.sh working on ascii art for ops

--- mpi_stuff/docker-cluster/utils.sh
+++ mpi_stuff/docker-cluster/utils.sh
@@ -10,6 +10,26 @@ export red="\e[1;31m"
 #        (.___\/{                                                                
 #~^~^~^~^~^~^~^~^~^~^~^~^~"
 
+export ASCII_ART_HEADER_BASE='                                                      
+                    ##        .                                                 
+              ## ## ##       ==                                                 
+           ## ## ## ##      ===                                                 
+       /""""""""""""""""\___/ ===                                               
+  ~~~ {~~ ~~~~ ~~~ ~~~~ ~~ ~ /  ===- ~~~                                        
+       \______ o          __/                                                   
+         \    \        __/                                                      
+          \____\______/                                                         
+                                                                                
+          |          |                                                          
+       __ |  __   __ | _  __   _                                                
+      /  \| /  \ /   |/  / _\ |                                                 
+      \__/| \__/ \__ |\_ \__  |                                            
+'
+
+export ASCII_ART_UP1=${ASCII_ART_HEADER_BASE}'    Bootsraping MPI Docker Cluster...'
+export ASCII_ART_UP2=${ASCII_ART_HEADER_BASE}'       MPI Docker Cluster Ready...'
+export ASCII_ART_DOWN=${ASCII_ART_HEADER_BASE}'    Tearing down MPI Docker Cluster...'
+
 export ASCII_ART_HEADER1='
                     ##        .
               ## ## ##       ==
@@ -25,23 +45,5 @@ export ASCII_ART_HEADER1='
       /  \| /  \ /   |/  / _\ |
       \__/| \__/ \__ |\_ \__  |
 
-    Bootsraping MPI Docker Cluster...
-'
-
-export ASCII_ART_HEADER2='                                                       
-                    ##        .                                                 
-              ## ## ##       ==                                                 
-           ## ## ## ##      ===                                                 
-       /""""""""""""""""\___/ ===                                               
-  ~~~ {~~ ~~~~ ~~~ ~~~~ ~~ ~ /  ===- ~~~                                        
-       \______ o          __/                                                   
-         \    \        __/                                                      
-          \____\______/                                                         
-                                                                                
-          |          |                                                          
-       __ |  __   __ | _  __   _                                                
-      /  \| /  \ /   |/  / _\ |                                                 
-      \__/| \__/ \__ |\_ \__  |                                                 
-                                                                                
-       MPI Docker Cluster Ready...
+    Tearing down MPI Docker Cluster...
 '
