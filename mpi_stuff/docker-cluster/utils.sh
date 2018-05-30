5180c2dc97aac6d9758eacbb650b6b38be892f2e
alejandrox1 <alarcj137@gmail.com>
2018-05-30 14:55:09 -0500

    mpi_stuff/docker-cluster/utils.sh the ascii art looks good

--- mpi_stuff/docker-cluster/utils.sh
+++ mpi_stuff/docker-cluster/utils.sh
@@ -2,11 +2,28 @@
 
 export reset="\e[0m"                                                            
 export blue="\e[1;34m"                                                          
-export green="\e[32m"                                                           
+export green="\e[1;42m"                                                           
 export red="\e[1;31m"
 
-export ASCII_ART_HEADER="                                                              
-         __v_                                                                   
-        (.___\/{                                                                
-~^~^~^~^~^~^~^~^~^~^~^~^~
-"
+#export ASCII_ART_HEADER="                                                              
+#         __v_                                                                   
+#        (.___\/{                                                                
+#~^~^~^~^~^~^~^~^~^~^~^~^~"
+
+export ASCII_ART_HEADER='
+                    ##        .
+              ## ## ##       ==
+           ## ## ## ##      ===
+       /""""""""""""""""\___/ ===
+  ~~~ {~~ ~~~~ ~~~ ~~~~ ~~ ~ /  ===- ~~~
+       \______ o          __/
+         \    \        __/
+          \____\______/
+
+          |          |
+       __ |  __   __ | _  __   _
+      /  \| /  \ /   |/  / _\ |
+      \__/| \__/ \__ |\_ \__  |
+
+         MPI Docker Cluster      
+'
