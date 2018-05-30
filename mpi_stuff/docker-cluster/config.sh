808c65bcbaf00583c22556baa81e64b7dc81789e
alejandrox1 <alarcj137@gmail.com>
2018-05-30 14:14:49 -0500

    mpi_stuff/docker-cluster/config.sh script with important configurations

---
+++ mpi_stuff/docker-cluster/config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export CLUSTER_REGISTRY_ADDR=localhost                                                 
+export CLUSTER_REGISTRY_PORT=5000                                                      
+export CLUSTER_SSH_PORT=2222                                                                   
+export IMAGE_NAME=mpi 
