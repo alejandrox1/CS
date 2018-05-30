0c1ef2e56bcddceff5836bc1bb758115ca898bb0
alejandrox1 <alarcj137@gmail.com>
2018-05-30 14:14:12 -0500

    mpi_stuff/docker-cluster/utils.sh script with utility setups (i.e., color vars, ascii art)

---
+++ mpi_stuff/docker-cluster/utils.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export reset="\e[0m"                                                            
+export blue="\e[1;34m"                                                          
+export green="\e[32m"                                                           
+export red="\e[1;31m"
+
+export ASCII_ART_HEADER="                                                              
+         __v_                                                                   
+        (.___\/{                                                                
+~^~^~^~^~^~^~^~^~^~^~^~^~
+"
