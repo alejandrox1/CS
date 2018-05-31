a507b727f78eb2f77b86956c338ee860061055f7
alejandrox1 <alarcj137@gmail.com>
2018-05-31 10:33:34 -0500

    mpi_stuff/docker-cluster/cluster.sh added down_master and down_workers methods

--- mpi_stuff/docker-cluster/cluster.sh
+++ mpi_stuff/docker-cluster/cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #-
 #-                       :-) A MPI Container Cluster (-:
+#-
 #- Adapted from alpine-mpich: https://github.com/NLKNguyen/alpine-mpich
 #-
 ##
@@ -50,6 +51,16 @@ scale_workers() {
     docker-compose up -d --scale worker=${num_workers}
 }
 
+down_master() {
+    echo -e "${green}docker-compose stop master && docker-compose rm -f master${reset}"
+    docker-compose stop master && docker-compose rm -f master
+}
+
+down_workers() {
+    echo -e "${green}docker-compose stop worker && docker-compose rm -f worker${reset}"
+    docker-compose stop worker && docker-compose rm -f worker
+}
+
 exec_on_mpi_master() {
     docker exec -it -u mpi $(docker-compose ps | grep 'master'| awk '{print $1}') "$@"
 }
@@ -57,6 +68,8 @@ exec_on_mpi_master() {
 cmd_up=0
 cmd_down=0
 cmd_login=0
+cmd_scale=0
+NUM_WORKERS=0
 
 # Parse command line arguments.                                                 
 while [[ "$#" > 0 ]]; do
@@ -80,6 +93,11 @@ while [[ "$#" > 0 ]]; do
         login)
             cmd_login=1
             ;;
+        -s|-scale)
+            cmd_scale=1
+            NUM_WORKERS="$2"
+            shift
+            ;;
         *)
             echo "Unknown command-line option: '${arg}'."
             exit 1
@@ -97,7 +115,7 @@ if [ $cmd_up == 1 ]; then
     generate_ssh_keys
     build_and_push_images
     up_master
-    scale_workers 3
+    scale_workers 1
 
     echo -e "${blue}${ASCII_ART_UP2}${reset}"
 
@@ -106,7 +124,15 @@ elif [ $cmd_down == 1 ]; then
     down_all
 
 elif [ $cmd_login == 1 ]; then
+    echo -e "${blue}${ASCII_ART_LOGIN}${reset}"
     exec_on_mpi_master ash
+
+elif [ $cmd_scale == 1 ]; then
+    echo -e "${blue}${ASCII_ART_SCALE}${reset}"
+    down_master
+    down_workers
+    up_master
+    scale_workers $NUM_WORKERS
 
 else
     echo whats gucci?
