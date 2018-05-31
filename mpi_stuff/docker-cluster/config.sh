739a6919223c1c3f1156c3368017edf7a61e1e40
alejandrox1 <alarcj137@gmail.com>
2018-05-31 10:35:25 -0500

    mpi_stuff/docker-cluster/config.sh added comments

--- mpi_stuff/docker-cluster/config.sh
+++ mpi_stuff/docker-cluster/config.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Local container image registry configurations.
 export CLUSTER_REGISTRY_ADDR=localhost                                                 
 export CLUSTER_REGISTRY_PORT=5000                                                      
 export CLUSTER_REGISTRY="${CLUSTER_REGISTRY_ADDR}:${CLUSTER_REGISTRY_PORT}"
+
+# Master ssh port.
 export CLUSTER_SSH_PORT=2222                                                                   
+
+# Name of Container image to be used for master/workers.
 export IMAGE_NAME=mpi 
