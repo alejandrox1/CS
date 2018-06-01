c1e258d608b915c19febe06cd622956ee07e31dd
alejandrox1 <alarcj137@gmail.com>
2018-05-31 21:45:20 -0500

    mpi_stuff/docker-cluster/mpi-node_image/auto_update_hosts.sh script to auto update host file

---
+++ mpi_stuff/docker-cluster/mpi-node_image/auto_update_hosts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# auto_update_hosts.
+#
+# Pass as argument the system's host file. Use this value to update the hosts
+# obtained from `get_host`.
+#
+
+hosts=$(get_hosts)
+echo "$hosts" > "$1"
+
+while sleep 2; do
+    current_hosts=$(get_hosts)
+    [ "$hosts" != "$current_hosts" ] && echo "$current_hosts" > "$1"
+    hosts=$current_hosts
+done
