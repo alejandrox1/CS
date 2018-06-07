98a01a251ab456d9011550d7581cd52fd52553ec
alejandrox1 <alarcj137@gmail.com>
2018-06-07 15:01:51 -0500

    HPP/KNC/ch02-test_drive/single_thread/helloflops1.c formated file

--- HPP/KNC/ch02-test_drive/single_thread/helloflops1.c
+++ HPP/KNC/ch02-test_drive/single_thread/helloflops1.c
@@ -10,14 +10,14 @@
 #include <string.h>
 #include <sys/time.h>
 
-#define FLOPS_ARRAY_SIZE (1024*1024)
+#define FLOPS_ARRAY_SIZE (1024 * 1024)
 #define MAXFLOPS_ITER 100000000
 #define LOOP_COUNT 128
 // Fused Multiply Add (FMA)
 #if defined(__FMA__) && defined(__AVX__)
-# define FLOPSPERCALC 2
+#define FLOPSPERCALC 2
 #else
-# define FLOPSPERCALC 1
+#define FLOPSPERCALC 1
 #endif
 
 // 64 byte aligned for fast cache access.
@@ -26,17 +26,16 @@ float fb[FLOPS_ARRAY_SIZE] __attribute__((aligned(64)));
 
 double dtime();
 
-
 int main(void)
 {
-    int i, j, k;
+    int    i, j, k;
     double tstart, tstop, ttime;
     double gflops = 0.0;
-    float a = 1.1;
+    float  a      = 1.1;
 
     /* Initialize the compute arrays. */
     printf("Initializing...\n");
-    for (i=0; i<FLOPS_ARRAY_SIZE; ++i)
+    for (i = 0; i < FLOPS_ARRAY_SIZE; ++i)
     {
         fa[i] = (float)i + 0.1;
         fb[i] = (float)i + 0.2;
@@ -46,10 +45,10 @@ int main(void)
 
     tstart = dtime();
     // Loop many times to get some flops.
-    for (j=0; j<MAXFLOPS_ITER; ++j)
+    for (j = 0; j < MAXFLOPS_ITER; ++j)
     {
         // Scale fa and add fb.
-        for (k=0; k<LOOP_COUNT; ++k)
+        for (k = 0; k < LOOP_COUNT; ++k)
             fa[k] = a * fa[k] + fb[k];
     }
     tstop = dtime();
@@ -62,11 +61,12 @@ int main(void)
     /* Output results. */
     if ((ttime) > 0)
         printf("GFlop = %10.3lf, Secs = %10.3lf, GFlop/s = %10.3lf\n",
-                gflops, ttime, gflops/ttime);
+               gflops,
+               ttime,
+               gflops / ttime);
 
     exit(EXIT_SUCCESS);
 }
-
 
 /**
  * dtime - returns current wall clock time.
