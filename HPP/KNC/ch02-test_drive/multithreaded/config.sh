153e316c48eda3efffbc4646df0ec2b543029217
alejandrox1 <alarcj137@gmail.com>
2018-06-07 16:11:57 -0500

    HPP/KNC/ch02-test_drive/multithreaded/config.sh -openmp is deprecated, use -qopenmp

--- HPP/KNC/ch02-test_drive/multithreaded/config.sh
+++ HPP/KNC/ch02-test_drive/multithreaded/config.sh
@@ -6,13 +6,13 @@ export MAKE=${MAKE:-make}
 if command -v icc > /dev/null 2>&1; then    # Intel compilation configuration.
     export CC=icc
     if [ "${NO_OPTIMIZATION}" == "true" ]; then
-        export CFLAGS='-no-vec -no-simd -openmp'
+        export CFLAGS='-no-vec -no-simd -qopenmp'
 
     elif [ "${KNL}" == "true" ] || [ "${SKYLAKE}" == "true" ]; then
-        export CFLAGS='-xMIC-AVX512 -qopt-report=3 -O3 -openmp'
+        export CFLAGS='-xMIC-AVX512 -qopt-report=3 -O3 -qopenmp'
 
     elif [ "${HASWELL}" == "true" ]; then
-        export CFLAGS='-xAVX -qopt-report=3 -O3 -openmp'
+        export CFLAGS='-xAVX -qopt-report=3 -O3 -qopenmp'
     fi
 elif command -v gcc > /dev/null 2>&1; then  # GCC compilation configuration.
     export CC=gcc
