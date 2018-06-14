dea47938bf83a67a8b056587a5ab1bc2b60f96e2
alejandrox1 <alarcj137@gmail.com>
2018-06-14 11:35:28 -0500

    GNU/autotools-tutorials/00-hello_world/src/main.c updates to sample app

--- GNU/autotools-tutorials/00-hello_world/src/main.c
+++ GNU/autotools-tutorials/00-hello_world/src/main.c
@@ -1,9 +1,9 @@
-//#include <config.h>
+#include <config.h>
 #include <stdio.h>
 
 int main(void)
 {
-    char PACKAGE_STRING[] = "hello-v0.0.0";
+    char PACKAGE_STRING[] = "amhello-1.0";
     puts("Hello world!");
     printf("This is package %s\n", PACKAGE_STRING);
     return 0;
