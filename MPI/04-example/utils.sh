2de3c09eea29af4310e1fbf3b48ee29c2e16e484
alejandrox1 <alarcj137@gmail.com>
2018-06-19 10:50:10 -0500

    MPI/04-example/*.{sh,yaml} add utility scripts for bootstraping the MPI Docker cluster
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

---
+++ MPI/04-example/utils.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export reset="\e[0m"                                                            
+export blue="\e[1;34m"                                                          
+export green="\e[1;42m"                                                           
+export red="\e[1;31m"
+
+#export ASCII_ART_HEADER="                                                              
+#         __v_                                                                   
+#        (.___\/{                                                                
+#~^~^~^~^~^~^~^~^~^~^~^~^~"
+
+export ASCII_ART_HEADER_BASE='                                                      
+                    ##        .                                                 
+              ## ## ##       ==                                                 
+           ## ## ## ##      ===                                                 
+       /""""""""""""""""\___/ ===                                               
+  ~~~ {~~ ~~~~ ~~~ ~~~~ ~~ ~ /  ===- ~~~                                        
+       \______ o          __/                                                   
+         \    \        __/                                                      
+          \____\______/                                                         
+                                                                                
+          |          |                                                          
+       __ |  __   __ | _  __   _                                                
+      /  \| /  \ /   |/  / _\ |                                                 
+      \__/| \__/ \__ |\_ \__  |                                            
+'
+
+export ASCII_ART_UP1=${ASCII_ART_HEADER_BASE}'    Bootsraping MPI Docker Cluster...'
+export ASCII_ART_UP2=${ASCII_ART_HEADER_BASE}'       MPI Docker Cluster Ready...'
+export ASCII_ART_DOWN=${ASCII_ART_HEADER_BASE}'    Tearing down MPI Docker Cluster...'
+export ASCII_ART_LOGIN=${ASCII_ART_HEADER_BASE}'   Loging in to the MPI Docker Cluster...'
+export ASCII_ART_EXEC=${ASCII_ART_HEADER_BASE}' Executing command on MPI Docker Cluster...'
+export ASCII_ART_SCALE=${ASCII_ART_HEADER_BASE}'     Scaling MPI Docker Cluster...'
