d8bce5fdce505e1c78ea6f758f98e9a58301fda4
alejandrox1 <alarcj137@gmail.com>
2018-06-22 15:34:49 -0500

    VM/mac/src/mac.c mac vm implementation
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

---
+++ VM/mac/src/mac.c
@@ -0,0 +1,101 @@
+/*
+ * mac.c
+ *
+ * Simple VM implemnetation.
+ *
+ * Taken from https://felixangell.com/blog/implementing-a-virtual-machine-in-c/
+ */
+#include <stdbool.h>
+#include <stdio.h>
+
+bool running = true;
+int ip = 0;
+int sp = -1;
+
+int stack[256];
+
+typedef enum
+{
+    PSH,
+    ADD,
+    POP,
+    SET,
+    HLT
+} InstructionSet;
+
+
+const int program[] =
+{
+    PSH, 5,
+    PSH, 6,
+    ADD,
+    POP,
+    HLT
+};
+
+void eval(int instr);
+int  fetch();
+
+int main(void)
+{
+    while (running)
+    {
+        eval(fetch());
+        ++ip;
+    }
+
+    return 0;
+}
+
+/*
+ * eval - Evaulate instruction.
+ */
+void eval(int instr)
+{
+    switch (instr)
+    {
+        case HLT: 
+        {
+            running = false;
+            printf("DONE\n");
+            break;
+        }
+
+        case PSH:
+        {
+            ++sp;
+            ++ip;
+            stack[sp] = program[ip];
+            break;
+        }
+
+        case POP:
+        {
+            int val_popped = stack[sp];
+            --sp;
+            printf("popped %d\n", val_popped);
+            break;
+        }
+
+        case ADD:
+        {
+            int a = stack[sp];
+            --sp;
+
+            int b = stack[sp];
+            --sp;
+
+            ++sp;
+            stack[sp] = a + b;
+            break;
+        }
+    }
+}
+
+/*
+ * fetch - Fetch an instruction from a program.
+ */
+int fetch()
+{
+    return program[ip];
+}
